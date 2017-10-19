fcf29143a6d5e621b3898759fd3d143e33392e19
Darien Fawkes <me@ileamare.ru>
2017-10-19 16:53:56 +0100

    Added .gitignore because why not

---
+++ rg_init.php
@@ -0,0 +1,15 @@
+<?php
+# TODO
+# init empty database
+# get matchids for league
+# clone
+# reload settings from database
+# reload settings to database
+# export to JSON
+# import from json
+
+# create database
+# create tables
+# check for reports folder
+
+ ?>
---
+++ update_metadata.php
@@ -0,0 +1,64 @@
+<?php
+  require_once("settings.php");
+
+  $conn = new mysqli($lrg_sql_host, $lrg_sql_user, $lrg_sql_pass, "d2_general_info");
+
+  if ($conn->connect_error) die("[F] Connection to SQL server failed: ".$conn->connect_error."\n");
+
+  $out = array();
+
+  $sql = "SELECT heroID, hero_tag, hero_locale_name FROM heroes;
+          SELECT itemID, itemTag FROM items;
+          SELECT lane_id, name FROM lanes;
+          SELECT modeID, modeName FROM gamemodes;";
+
+  if ($conn->multi_query($sql)) {
+    $res = $conn->store_result();
+
+    $out['heroes'] = array();
+    while ($row = $res->fetch_row()) {
+      $out['heroes'][$row[0]] = array(
+        "tag"    => $row[1],
+        "name"   => $row[2]
+      );
+    }
+    $res->free();
+
+    $conn->next_result();
+
+    $res = $conn->store_result();
+
+    $out['items'] = array();
+    while ($row = $res->fetch_row()) {
+      $out['items'][$row[0]] = $row[1];
+    }
+    $res->free();
+
+    $conn->next_result();
+
+    $res = $conn->store_result();
+
+    $out['lanes'] = array();
+    while ($row = $res->fetch_row()) {
+      $out['lanes'][$row[0]] = $row[1];
+    }
+    $res->free();
+
+    $conn->next_result();
+
+    $res = $conn->store_result();
+
+    $out['modes'] = array();
+    while ($row = $res->fetch_row()) {
+      $out['modes'][$row[0]] = $row[1];
+    }
+    $res->free();
+
+    $filename = "res/metadata.json";
+    $f = fopen($filename, "w") or die("[F] Couldn't open file to save results. Check working directory for `reports` folder.\n");
+    fwrite($f, json_encode($out));
+    fclose($f);
+  }
+
+
+ ?>
