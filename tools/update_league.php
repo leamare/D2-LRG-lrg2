6c2e324da65930252da254004464c08672131089
Leamare <me@ileamare.ru>
2018-01-23 01:01:03 +0300

    update "update league" tool, add "remove match" tool (issue #17 resolved)

--- tools/update_league.php
+++ tools/update_league.php
@@ -6,17 +6,14 @@ $reports = scandir("leagues");
 
 if (!file_exists("templates/default.json")) die("[F] No default league template found, exitting.");
 
-$lg_settings = json_decode(file_get_contents("templates/default.json"), true);
+$def_settings = json_decode(file_get_contents("templates/default.json"), true);
 
-$old = file_get_contents("leagues/".$lrg_league_tag, "r") or die("[F] Couldn't open league file.\n");
-$old = json_decode($old, true);
-
-migrate_params($lg_settings, $old);
+migrate_params($def_settings, $lg_settings);
 
 echo " OK";
 
 $f = fopen("leagues/".$lrg_league_tag.".json", "w+") or die("[F] Couldn't open file to save results. Check working directory for `reports` folder.\n");
-fwrite($f, json_encode($lg_settings));
+fwrite($f, json_encode($def_settings));
 fclose($f);
 
 echo " OK\n";
