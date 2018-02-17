e48a524f334aa9e010e687b5b3d332131c8aa6d9
Leamare <me@ileamare.ru>
2018-02-17 20:49:02 +0300

    fix remove match tool

--- tools/remove_match.php
+++ tools/remove_match.php
@@ -1,24 +1,27 @@
 <?php
 require_once("settings.php");
 
-$options = getopt("m", [ "match" ]);
+$options = getopt("m:", [ "match" ]);
 
-if(isset($opdions['m']))
-  $mid = (int)$options['m'];
-else die("[F] No match ID specified\n")
+if(isset($options['m']))
+  $mid = $options['m'];
+else die("[F] No match ID specified\n");
 
 
+
+$conn = new mysqli($lrg_sql_host, $lrg_sql_user, $lrg_sql_pass, $lrg_sql_db);
 
 $sql = "DELETE from matchlines where matchid = $mid; DELETE from adv_matchlines where matchid = $mid; ".
        "DELETE from draft where matchid = $mid; ".
        ( $lg_settings['main']['teams'] ? "delete from teams_matches where matchid = $mid;" : "").
        "delete from matches where matchid = $mid;";
 
+       
 if ($conn->multi_query($sql) === TRUE) echo "[S] Removed matches.\n";
 else die("[F] Unexpected problems when quering database.\n".$conn->error."\n");
 
 $query_res = $conn->store_result();
-$query_res->free_result();
+//$query_res->free_result();
 
 echo "OK \n";
 
