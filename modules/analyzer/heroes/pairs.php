d6b43680837d72610a1634e252706bdb86d9d825
Ilia Silakov <me@ileamare.ru>
2018-07-10 19:14:09 +0300

    add lane rate for pairs

--- modules/analyzer/heroes/pairs.php
+++ modules/analyzer/heroes/pairs.php
@@ -1,11 +1,21 @@
 <?php
 $result["hero_pairs"] = array();
 
-$sql = "SELECT m1.heroid, m2.heroid, COUNT(distinct m1.matchid) match_count, SUM(NOT matches.radiantWin XOR m1.isRadiant)/SUM(1) winrate
-        FROM matchlines m1 JOIN matchlines m2
-          ON m1.matchid = m2.matchid and m1.isRadiant = m2.isRadiant and m1.heroid < m2.heroid
-          JOIN matches ON m1.matchid = matches.matchid
-        GROUP BY m1.heroid, m2.heroid
+$sql = "SELECT fm1.heroid, fm2.heroid,
+          COUNT(distinct fm1.matchid) match_count,
+          SUM(NOT matches.radiantWin XOR fm1.isRadiant)/SUM(1) winrate,
+          SUM(fm1.lane = fm2.lane)/SUM(1) lane_rate
+        FROM
+          ( select m1.matchid, m1.heroid, am1.lane, m1.isRadiant
+            from matchlines m1 JOIN adv_matchlines am1
+            ON m1.matchid = am1.matchid AND m1.heroid = am1.heroid ) fm1
+        JOIN
+          ( select m2.matchid, m2.heroid, am2.lane, m2.isRadiant
+            from matchlines m2 JOIN adv_matchlines am2
+            ON m2.matchid = am2.matchid AND m2.heroid = am2.heroid ) fm2
+        ON fm1.matchid = fm2.matchid and fm1.isRadiant = fm2.isRadiant and fm1.heroid < fm2.heroid
+        JOIN matches ON fm1.matchid = matches.matchid
+        GROUP BY fm1.heroid, fm2.heroid
         HAVING match_count > $limiter
         ORDER BY match_count DESC, winrate DESC;";
 # limiting match count for hero pair to 3:
@@ -18,21 +28,17 @@ else die("[F] Unexpected problems when requesting database.\n".$conn->error."\n"
 $query_res = $conn->store_result();
 
 for ($row = $query_res->fetch_row(); $row != null; $row = $query_res->fetch_row()) {
-  //$result['random']['matches_total']
-  //$result['pickban'][]['matches_picked']
   $hero1_pickrate = $result['pickban'][$row[0]]['matches_picked'] / $result['random']['matches_total'];
   $hero2_pickrate = $result['pickban'][$row[1]]['matches_picked'] / $result['random']['matches_total'];
   $expected_pair  = $hero1_pickrate * $hero2_pickrate * ($result['random']['matches_total']/2);
-  $pair_percentage = $row[2] / $expected_pair;
-
-  //if($pair_percentage < 0.05) continue;
 
   $result["hero_pairs"][] = array (
     "heroid1" => $row[0],
     "heroid2" => $row[1],
     "matches" => $row[2],
     "winrate" => $row[3],
-    "expectation" => $expected_pair
+    "expectation" => $expected_pair,
+    "lane_rate" => $row[4]
   );
 }
 
