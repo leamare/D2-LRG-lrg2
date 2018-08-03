a32e2dff9198774f64827bdca20b1178f743b567
Ilia Silakov <me@ileamare.ru>
2018-08-03 04:21:54 +0300

    change player_name() to display player ID if there's no players data

--- modules/view/functions/player_name.php
+++ modules/view/functions/player_name.php
@@ -2,12 +2,16 @@
 function player_name($pid) {
   global $report;
   $res = "";
-  if($pid && isset($report['players'][$pid])) {
-    if(isset($report['teams'])) {
-      $res .= team_tag($report['players_additional'][$pid]['team']).".";
-    }
-  }
+  if (isset($report['players'])) {
+    if($pid && isset($report['players'][$pid])) {
+      if(isset($report['teams'])) {
+        $res .= team_tag($report['players_additional'][$pid]['team']).".";
+      }
       return $res.htmlspecialchars($report['players'][$pid]);
-  return "null";
+    }
+    return "null";
+  } else {
+    return "PID $pid";
+  }
 }
 ?>
