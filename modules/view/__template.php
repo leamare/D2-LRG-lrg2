914679005808ca2854b3f9b19af7417fab06dab9
Ilia Silakov <me@ileamare.ru>
2019-06-08 18:48:35 +0300

    minor template improvements

--- modules/view/__template.php
+++ modules/view/__template.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="<?php echo $locale; ?>">
   <head>
     <!--
       League Report Generator
@@ -10,6 +10,10 @@
        if(file_exists("res/favicon.ico")) echo "<link rel=\"shortcut icon\" href=\"res/favicon.ico\" />";
     ?>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description" content="Dota 2 Statistics hub at Spectral.GG, made by Leamare<?php
+      if (isset($report['league_name'])) 
+        echo ", ".$report['league_name']." report"; ?>">
     <title><?php
       echo $instance_title;
       if (!empty($leaguetag))
@@ -30,19 +34,7 @@
           }
           if(isset($custom_logo))
               echo "<link href=\"res/custom_styles/logos/".$custom_logo.".css\" rel=\"stylesheet\" type=\"text/css\" />";
-          if($use_graphjs) {
-            echo "<script type=\"text/javascript\" src=\"res/dependencies/Chart.bundle.min.js\"></script>";
-          }
-          if($use_visjs) {
-            echo "<script type=\"text/javascript\" src=\"res/dependencies/vis.min.js\"></script>";
-            echo "<script type=\"text/javascript\" src=\"res/dependencies/vis-network.min.js\"></script>";
-            echo "<link href=\"res/dependencies/vis.min.css\" rel=\"stylesheet\" type=\"text/css\" />";
-            echo "<link href=\"res/dependencies/vis-network.min.css\" rel=\"stylesheet\" type=\"text/css\" />";
-          }
-
      if (!empty($custom_head)) echo $custom_head; ?>
-     <script type="text/javascript" src="res/dependencies/jquery.min.js"></script>
-     <script type="text/javascript" src="res/dependencies/jquery.tablesorter.min.js"></script>
   </head>
   <body>
     <?php if (!empty($custom_body)) echo $custom_body; ?>
@@ -71,7 +63,7 @@
       <div class="locale-changer">
         <?php
         $link = substr($_SERVER['REQUEST_URI'], strrpos($_SERVER['REQUEST_URI'], "/")+1);
-          echo '<select onchange="select_modules_link(this);" class="select-locale">';
+          echo '<label><select onchange="select_modules_link(this);" class="select-locale">';
           foreach($locales as $loc => $lname) {
             $loc = str_replace(".json", "", $loc);
             if($loc == $locale)
@@ -85,7 +77,7 @@
                    )
                ).'">'.$lname.'</option>';
           }
-          echo '</select>';
+          echo '</select></label>';
 
         ?>
       </div>
@@ -123,7 +115,7 @@
         <a href="https://dota2.com" target="_blank" rel="noopener">Dota 2</a> is a registered trademark of <a href="https://valvesoftware.com" target="_blank" rel="noopener">Valve Corporation.</a>
         Match replay data provided by <a href="https://stratz.com/" target="_blank" rel="noopener">STRATZ</a> and <a href="https://opendota.com" target="_blank" rel="noopener">OpenDota</a>.<br />
         Graphs are made with <a href="https://visjs.org" target="_blank" rel="noopener">vis.js</a> and <a href="http://www.chartjs.org/" target="_blank" rel="noopener">chart.js</a>.<br />
-        Made by <a href="https://spectralalliance.ru" target="_blank" rel="noopener">Spectral Alliance</a>
+        Made by Leamare @ <a href="https://spectral.gg" target="_blank" rel="noopener">Spectral.GG</a>
         with support of <a href="https://vk.com/thecybersport" target="_blank" rel="noopener">TheCyberSport</a>. Klozi is a registered trademark of Grafensky.<br />
         <?php if (!empty($custom_footer)) echo $custom_footer."<br />";
           echo "LRG web version: <a>".parse_ver($lg_version)."</a>. ";
@@ -137,6 +129,19 @@
           <div id="modal-sublevel" class="modal-sublevel"></div>
         </div>
       </div>
+      <?php 
+      if($use_graphjs) {
+        echo "<script type=\"text/javascript\" src=\"res/dependencies/Chart.bundle.min.js\"></script>";
+      }
+      if($use_visjs) {
+        echo "<script type=\"text/javascript\" src=\"res/dependencies/vis.min.js\"></script>";
+        echo "<script type=\"text/javascript\" src=\"res/dependencies/vis-network.min.js\"></script>";
+        echo "<link href=\"res/dependencies/vis.min.css\" rel=\"stylesheet\" type=\"text/css\" />";
+        echo "<link href=\"res/dependencies/vis-network.min.css\" rel=\"stylesheet\" type=\"text/css\" />";
+      }
+      ?>
+      <script type="text/javascript" src="res/dependencies/jquery.min.js"></script>
+      <script type="text/javascript" src="res/dependencies/jquery.tablesorter.min.js"></script>
       <script type="text/javascript" src="res/reports.js"></script>
     </body>
   </html>
