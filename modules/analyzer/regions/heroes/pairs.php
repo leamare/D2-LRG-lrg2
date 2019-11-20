4983296ad2826538d1b43e75312a0b88fbd2a4b1
Eli Silakov <me@ileamare.ru>
2019-11-20 20:33:38 +0300

    fix hero pairs missing some matches

--- modules/analyzer/regions/heroes/pairs.php
+++ modules/analyzer/regions/heroes/pairs.php
@@ -2,16 +2,16 @@
 $result["regions_data"][$region]["hero_pairs"] = [];
 
 $sql = "SELECT fm1.heroid, fm2.heroid,
-          COUNT(distinct fm1.matchid) match_count,
+          COUNT(distinct matches.matchid) match_count,
           SUM(NOT matches.radiantWin XOR fm1.isRadiant) wins,
           SUM(fm1.lane = fm2.lane)/SUM(1) lane_rate
         FROM
           ( select m1.matchid, m1.heroid, am1.lane, m1.isRadiant
-            from matchlines m1 JOIN adv_matchlines am1
+            from matchlines m1 LEFT JOIN adv_matchlines am1
             ON m1.matchid = am1.matchid AND m1.heroid = am1.heroid ) fm1
         JOIN
           ( select m2.matchid, m2.heroid, am2.lane, m2.isRadiant
-            from matchlines m2 JOIN adv_matchlines am2
+            from matchlines m2 LEFT JOIN adv_matchlines am2
             ON m2.matchid = am2.matchid AND m2.heroid = am2.heroid ) fm2
         ON fm1.matchid = fm2.matchid and fm1.isRadiant = fm2.isRadiant and fm1.heroid < fm2.heroid
         JOIN matches ON fm1.matchid = matches.matchid
