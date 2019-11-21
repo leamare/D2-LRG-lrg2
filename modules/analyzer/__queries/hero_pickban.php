3bf391a50ef68ab9e3c6bc6fc78b600388d5823e
Eli Silakov <me@ileamare.ru>
2019-11-21 15:59:11 +0300

    add hero_pickban query executor

---
+++ modules/analyzer/__queries/hero_pickban.php
@@ -0,0 +1,70 @@
+<?php 
+
+function rg_query_hero_pickban($team = null, $cluster = null) {
+  $res = [];
+
+  $sql = "SELECT draft.hero_id hero_id, 
+    count(distinct draft.matchid) matches, 
+    SUM(NOT matches.radiantWin XOR draft.is_radiant)/count(distinct draft.matchid) winrate
+    FROM draft JOIN matches ON draft.matchid = matches.matchid ".
+    ($team === null ? "" : "JOIN teams_matches ON draft.matchid = teams_matches.matchid AND draft.is_radiant = teams_matches.is_radiant ").
+  " WHERE is_pick = true ".
+    ($cluster !== null ? " AND matches.cluster IN (".implode(",", $cluster).") " : "").
+    ($team === null ? "" : " AND teams_matches.teamid = $id ").
+  " GROUP BY draft.hero_id;";
+
+  if ($conn->multi_query($sql) === TRUE);
+  else die("[F] Unexpected problems when requesting database.\n".$conn->error."\n");
+
+  $query_res = $conn->store_result();
+
+  for ($row = $query_res->fetch_row(); $row != null; $row = $query_res->fetch_row()) {
+    $res[$row[0]] = array (
+    "matches_total"   => $row[1],
+    "matches_picked"  => $row[1],
+    "winrate_picked"  => $row[2],
+    "matches_banned"  => 0,
+    "winrate_banned"  => 0
+    );
+  }
+
+  $query_res->free_result();
+
+  $sql = "SELECT draft.hero_id hero_id, 
+    count(distinct draft.matchid) matches, 
+    SUM(NOT matches.radiantWin XOR draft.is_radiant)/count(distinct draft.matchid) winrate
+    FROM draft JOIN matches ON draft.matchid = matches.matchid ".
+    ($team === null ? "" : "JOIN teams_matches ON draft.matchid = teams_matches.matchid AND draft.is_radiant = teams_matches.is_radiant ").
+  " WHERE is_pick = false ".
+    ($cluster !== null ? " AND matches.cluster IN (".implode(",", $cluster).") " : "").
+    ($team === null ? "" : " AND teams_matches.teamid = $id ").
+  " GROUP BY draft.hero_id;";
+
+  if ($conn->multi_query($sql) === TRUE);
+  else die("[F] Unexpected problems when requesting database.\n".$conn->error."\n");
+
+  $query_res = $conn->store_result();
+
+  for ($row = $query_res->fetch_row(); $row != null; $row = $query_res->fetch_row()) {
+  if(isset($res[$row[0]])) {
+    $res[$row[0]] = array (
+      "matches_total"   => ($res[$row[0]]["matches_total"]+$row[1]),
+      "matches_picked"  => $res[$row[0]]["matches_picked"],
+      "winrate_picked"  => $res[$row[0]]["winrate_picked"],
+      "matches_banned"  => $row[1],
+      "winrate_banned"  => $row[2]
+    );
+  } else
+    $res[$row[0]] = array (
+      "matches_total"   => $row[1],
+      "matches_picked"  => 0,
+      "winrate_picked"  => 0,
+      "matches_banned"  => $row[1],
+      "winrate_banned"  => $row[2]
+    );
+  }
+
+  $query_res->free_result();
+
+  return $res;
+}
--- modules/analyzer/regions/heroes/pickban.php
+++ modules/analyzer/regions/heroes/pickban.php
@@ -1,57 +1,5 @@
 <?php
-$result["regions_data"][$region]["pickban"] = [];
 
-$sql = "SELECT draft.hero_id hero_id, SUM(1) matches, SUM(NOT matches.radiantWin XOR draft.is_radiant)/SUM(1) winrate
-	   FROM draft JOIN matches ON draft.matchid = matches.matchid
-	   WHERE is_pick = true AND matches.cluster IN (".implode(",", $clusters).")
-	   GROUP BY draft.hero_id;";
+$result["regions_data"][$region]["pickban"] = rg_query_hero_pickban(null, $clusters);
 
-if ($conn->multi_query($sql) === TRUE);
-else die("[F] Unexpected problems when requesting database.\n".$conn->error."\n");
-
-$query_res = $conn->store_result();
-
-for ($row = $query_res->fetch_row(); $row != null; $row = $query_res->fetch_row()) {
-  $result["regions_data"][$region]["pickban"][$row[0]] = array (
-    "matches_total"   => $row[1],
-    "matches_picked"  => $row[1],
-    "winrate_picked"  => $row[2],
-    "matches_banned"  => 0,
-    "winrate_banned"  => 0
-  );
-}
-
-$query_res->free_result();
-
-$sql = "SELECT draft.hero_id hero_id, SUM(1) matches, SUM(NOT matches.radiantWin XOR draft.is_radiant)/SUM(1) winrate
-	   FROM draft JOIN matches ON draft.matchid = matches.matchid
-	   WHERE is_pick = false AND matches.cluster IN (".implode(",", $clusters).")
-	   GROUP BY draft.hero_id
-ORDER BY winrate DESC, matches DESC;";
-
-if ($conn->multi_query($sql) === TRUE);
-else die("[F] Unexpected problems when requesting database.\n".$conn->error."\n");
-
-$query_res = $conn->store_result();
-
-for ($row = $query_res->fetch_row(); $row != null; $row = $query_res->fetch_row()) {
-  if(isset($result["regions_data"][$region]["pickban"][$row[0]])) {
-    $result["regions_data"][$region]["pickban"][$row[0]] = array (
-      "matches_total"   => ($result["regions_data"][$region]["pickban"][$row[0]]["matches_total"]+$row[1]),
-      "matches_picked"  => $result["regions_data"][$region]["pickban"][$row[0]]["matches_picked"],
-      "winrate_picked"  => $result["regions_data"][$region]["pickban"][$row[0]]["winrate_picked"],
-      "matches_banned"  => $row[1],
-      "winrate_banned"  => $row[2]
-    );
-  } else
-    $result["regions_data"][$region]["pickban"][$row[0]] = array (
-      "matches_total"   => $row[1],
-      "matches_picked"  => 0,
-      "winrate_picked"  => 0,
-      "matches_banned"  => $row[1],
-      "winrate_banned"  => $row[2]
-    );
-}
-
-$query_res->free_result();
 ?>
