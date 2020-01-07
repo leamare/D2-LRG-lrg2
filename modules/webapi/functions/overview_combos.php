6974dc6174b0f83a6daad40b683292d800b83953
Eli Silakov <me@ileamare.ru>
2020-01-08 01:25:41 +0300

    LRG2API functions overview_combos

---
+++ modules/webapi/functions/overview_combos.php
@@ -0,0 +1,24 @@
+<?php 
+
+function rgapi_generator_overview_combos($table_id, $caption, $context, $limiter = 10, $heroes_flag = true) {
+  if(empty($context)) return [];
+
+  # Figuring out what kind of context we have here
+  $i = $limiter;
+
+  uasort($context, function($a, $b) {
+    $dev_a = $a['matches']-$a['expectation'];
+    $dev_b = $b['matches']-$b['expectation'];
+    if($dev_a == $dev_b) return 0;
+    return ($dev_a < $dev_b) ? 1 : -1;
+  });
+
+  $res = [];
+  foreach($context as $combo) {
+    $i--;
+    $res[] = $combo;
+    if($i < 0) break;
+  }
+
+  return $res;
+}
