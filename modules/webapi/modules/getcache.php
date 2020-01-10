ec4ce531ec87b7f290fdab11cc275633a1a514b9
Eli Silakov <me@ileamare.ru>
2020-01-10 04:53:39 +0300

    webapi cache improvements

--- modules/webapi/modules/getcache.php
+++ modules/webapi/modules/getcache.php
@@ -1,6 +1,6 @@
 <?php 
 
-$endpoints['getcache'] = function($mods, $vars, &$report) use ($cache_file, $lg_version) {
+$endpoints['getcache'] = function($mods, $vars, &$report) use ($cache_file, $lg_version, $reports_dir, $report_mask) {
   $lightcache = true;
   include_once(__DIR__ . "/../../view/__open_cache.php");
   include_once(__DIR__ . "/../../view/__update_cache.php");
