0544ca693003d487b66b0fef08e0f7f201889f2d
Eli Silakov <me@ileamare.ru>
2020-10-21 23:32:27 +0300

    multiple link providers support

--- modules/view/functions/links.php
+++ modules/view/functions/links.php
@@ -1,29 +1,40 @@
 <?php
 
 function match_link($mid) {
-  global $link_provider;
+  global $link_provider, $links_providers;
+
+  if (empty($links_providers))
   return "<a href=\"https://$link_provider/matches/$mid\" target=\"_blank\" rel=\"noopener\">$mid</a>";
+  
+  $r = $mid." - ";
+  foreach ($links_providers as $lpn => $lpl) {
+    $r .= "<a target=\"_blank\" href=\"https://$lpl/matches/$mid\">".link_provider($lpn)."</a> ";
+  }
+  //$r .= " ] ";
+  return $r;
+}
+
+function link_provider($lpn) {
+  global $link_provider_icon;
+  if (empty($link_provider_icon)) return $lpn;
 }
 
 function team_link($tid) {
-  global $leaguetag;
-  global $linkvars;
+  global $leaguetag, $linkvars;
 
   return "<a href=\"?league=".$leaguetag."&mod=teams-profiles-team".$tid.(empty($linkvars) ? "" : "&$linkvars")
     ."\" title=\"".team_name($tid)."\">".team_name($tid)." (".team_tag($tid).")</a>";
 }
 
 function region_link($rid) {
-  global $leaguetag;
-  global $linkvars;
+  global $leaguetag, $linkvars;
 
   return "<a href=\"?league=".$leaguetag."&mod=regions-region$rid".(empty($linkvars) ? "" : "&$linkvars")
     ."\" title=\"".locale_string("region$rid")."\">".locale_string("region$rid")."</a>";
 }
 
 function team_link_short($tid) {
-  global $leaguetag;
-  global $linkvars;
+  global $leaguetag, $linkvars;
 
   return "<a href=\"?league=".$leaguetag."&mod=teams-profiles-team".$tid.(empty($linkvars) ? "" : "&$linkvars")
     ."\" title=\"".team_name($tid)."\">".team_tag($tid)."</a>";
--- modules/view/functions/player_name.php
+++ modules/view/functions/player_name.php
@@ -18,13 +18,17 @@ function player_name($pid, $tt = true) {
 
 function player_link($pid, $tt = true) {
   global $link_provider, $links_providers;
+
+  if ($pid < 0) return player_name($pid, $tt);
+
   if (empty($links_providers))
     return "<a target=\"_blank\" href=\"https://$link_provider/players/$pid\">".player_name($pid, $tt)."</a>";
   
-  $r = player_name($pid, $tt)." ";
+  $r = player_name($pid, $tt)." - ";
   foreach ($links_providers as $lpn => $lpl) {
-    $r .= "<a target=\"_blank\" href=\"https://$lpl/players/$pid\">$lpn</a> ";
+    $r .= "<a target=\"_blank\" href=\"https://$lpl/players/$pid\">".link_provider($lpn)."</a> ";
   }
+  //$r .= " ] ";
   return $r;
 }
 
