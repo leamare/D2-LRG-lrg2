32b6a453db409ef3e90b86baf638fcd8a81eba1b
Eli Silakov <me@ileamare.ru>
2020-11-01 00:43:20 +0300

    update graph limiter to use a better formula

--- modules/analyzer/main/limiters.php
+++ modules/analyzer/main/limiters.php
@@ -4,8 +4,11 @@ $picks = [];
 foreach($result["pickban"] as $hero)
   $picks[] = (int)($hero["matches_picked"] ?? $hero["matches_total"]);
 
-function calculate_limiters(array $dataset, int $teams = null): array {
+function calculate_limiters(array $dataset, $teams = null, $total = null): array {
   if (!empty($dataset)) {
+    if (empty($total))
+      $total = max($dataset);
+
     $median = quantile($dataset, 0.5);
     $mean = expected($dataset);
     $sq_dev = sq_dev($dataset);
@@ -14,19 +17,31 @@ function calculate_limiters(array $dataset, int $teams = null): array {
     $q2 = 1-find_position($dataset, ($mean+$sq_dev)*2)/count($dataset);
     $q2m = 1-find_position($dataset, ($mean+$sq_dev)*1.85)/count($dataset);
     //echo "\n".(($mean+$sq_dev)*1.85)."\n"; die();
-    // graphs limiter isn't really good at M-2sd, so we need to use a value
-    // somewhere in the middle, 1.75 multiplier proved to be the best solution
-    // since it's pretty close to 10% treshold which I was using before
-    // It's kind of practically proven value
     $q3 = 1-find_position($dataset, ($mean+$sq_dev)*3)/count($dataset);
     // and then i screwed up all the maths
     // All these shenanigans didn't really change much for low match counts
     // but for higher match counts I've got a much better result by changing values into 1-Q((M+sd)*n)
     // and it's actually pretty much Q(M-sd*n), except it's easier to work with
+
+    $pairs = [];
+    foreach ($dataset as $i => $v1) {
+      foreach ($dataset as $j => $v2) {
+        if ($i == $j) continue;
+        $pairs[] = round($v1 * $v2 / $total);
+      }
+    }
+    sort($pairs);
+    // Graphs limiter is based on a different dataset
+    // that is basically representing expectations for all the possible pairs.
+    // Expectation * 2 to be exact.
+    // Quantile 0.8 is used to filter out top 20% of pairs (well, probably closer to top 10%)
+    // but anyway.
   } else {
     $median = 0;
     $sq_dev = 0;
     $q1 = 0; $q2 = 0; $q2m = 0; $q3 = 0;
+
+    $pairs = [ 0 ];
   }
 
   return [
@@ -35,14 +50,14 @@ function calculate_limiters(array $dataset, int $teams = null): array {
     "median" => unzero($median),
     "limiter_higher" => unzero(quantile($dataset, $q1)),
     "limiter_middle" => unzero(round(quantile($dataset, $q2))),
-    "limiter_graph" => unzero(round(quantile($dataset, $q2m))),
+    "limiter_graph" => unzero(round(quantile($pairs, 0.8))),
     "limiter_lower" => unzero(round($teams ? 
       ceil(quantile($dataset, $q1)/$teams) :
       quantile($dataset, $q3)))
   ];
 }
 
-$limiters = calculate_limiters($picks, $result['random']['teams_on_event'] ?? null);
+$limiters = calculate_limiters($picks, $result['random']['teams_on_event'] ?? null, $result['random']["matches_total"]);
 
 //compatibility
 $limiter = $limiters['limiter_higher'];
