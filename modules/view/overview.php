e9e49aca64212209b2dc913d0cada155bc2adaf0
Eli Silakov <me@ileamare.ru>
2020-12-12 23:00:19 +0100

    wv: reorder elements in overview section, add league banner

--- modules/view/overview.php
+++ modules/view/overview.php
@@ -5,22 +5,33 @@ $modules['overview'] = "";
 
 # It will be updated later to support team-centric reports
 function rg_view_generate_overview() {
-  global $report;
+  global $report, $league_logo_banner_provider;
 
   $res = "<div class=\"content-header\">".locale_string("summary")."</div>";
 
   $res .= "<div class=\"block-content\">";
 
   $res .= locale_string("over-pregen-report");
-  if ($report['league_id'] == null || $report['league_id'] == "custom")
+  if ($report['league_id'] == null || $report['league_id'] == "custom") {
     $res .= " ".locale_string("over-custom-league")." ".$report['league_name']." — ".$report['league_desc'].".";
-  else
+  } else {
     $res .= " ".$report['league_name']." (".$report['league_id'].") — ".$report['league_desc'].".";
+  }
 
   $res .= "</div>";
 
-  if (!empty($report['orgs'])) {
-    $res .= "<div class=\"block-content\"><a target=\"_blank\" href=\"".$report['orgs']."\">".locale_string("website_long")."</a></div>";
+  if ($report['league_id'] !== null && $report['league_id'] !== "custom") {
+    $res .= "<div class=\"block-content tournament-banner\"><img src=\"".str_replace('%LID%', $report['league_id'], $league_logo_banner_provider)."\" alt=\"".$report['league_id']."\" /></div>";
+  }
+
+  if (!empty($report['sponsors'])) {
+    $res .= "<div class=\"block-content\">".locale_string("sponsors").": ";
+    $links = [];
+    foreach($report['sponsors'] as $type => $link) {
+      $links[] = "<a target=\"_blank\" href=\"".$link."\">".$type."</a>";
+    }
+    $res .= implode(", ", $links);
+    $res .= "</div>";
   }
 
   if (!empty($report['links'])) {
@@ -33,14 +44,8 @@ function rg_view_generate_overview() {
     $res .= "</div>";
   }
 
-  if (!empty($report['sponsors'])) {
-    $res .= "<div class=\"block-content\">".locale_string("sponsors").": ";
-    $links = [];
-    foreach($report['sponsors'] as $type => $link) {
-      $links[] = "<a target=\"_blank\" href=\"".$link."\">".$type."</a>";
-    }
-    $res .= implode(", ", $links);
-    $res .= "</div>";
+  if (!empty($report['orgs'])) {
+    $res .= "<div class=\"block-content\"><a target=\"_blank\" href=\"".$report['orgs']."\">".locale_string("website_long")."</a></div>";
   }
 
   return rg_view_generator_overview("", $report, $res);
