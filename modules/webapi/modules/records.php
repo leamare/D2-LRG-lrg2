d31968ae582e1cf51a8374f47f1accabd72013d6
Eli Silakov <me@ileamare.ru>
2020-12-15 19:40:19 +0100

    wa: update records to use min match card and have a more comfort schema

--- modules/webapi/modules/records.php
+++ modules/webapi/modules/records.php
@@ -4,7 +4,40 @@ $endpoints['records'] = function($mods, $vars, &$report) {
   // parse mods for region ID
   // check if region persists
   if (isset($vars['region']) && isset($report['regions_data'][ $vars['region'] ])) {
-    return $report['regions_data'][ $vars['region'] ]['records'] ?? null;
+    $data = $report['regions_data'][ $vars['region'] ]['records'] ?? null;
+  } else $data = $report['records'] ?? null;
+
+  if (empty($data))
+    throw new \Exception("Problem occured when fetching records.");
+
+  $res = [];
+  
+  foreach ($data as $k => $v) {
+    if ($v['matchid'] && !empty($report['match_participants_teams']))
+      $v['match_card_min'] = match_card_min($v['matchid']);
+    else 
+      $v['match_card_min'] = null;
+
+    if (!$v['heroid'])
+      $v['heroid'] = null;
+
+    if (!$v['matchid'])
+      $v['matchid'] = null;
+
+    if ($v['playerid']) {
+      if (strpos($k, '_team')) {
+        $v['name'] = team_name($v['playerid']);
+        $v['teamid'] = $v['playerid'];
+        $v['playerid'] = null;
+      } else {
+        $v['name'] = player_name($v['playerid']);
+      }
+    } else {
+      $v['playerid'] = null;
+    }
+    
+    $res[$k] = $v;
   }
-  return $report['records'] ?? null;
+
+  return $res;
 };
