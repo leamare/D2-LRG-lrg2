4715a871bcb0a0f57880daaee944a80249e23e4c
Eli Silakov <me@ileamare.ru>
2021-01-18 03:27:46 +0300

    wv: fix draft accuracy test

--- modules/view/generators/draft.php
+++ modules/view/generators/draft.php
@@ -193,7 +193,7 @@ function rg_generator_draft($table_id, &$context_pickban, &$context_draft, $cont
 function rg_draft_accuracy_test(&$context_pickban, &$context_draft) {
   $ratios = [];
   foreach ($context_draft[1][3] as $dr) {
-    $total = $context_pickban[ $dr['heroid'] ]['matches_total'];
+    $total = $context_pickban[ $dr['heroid'] ]['matches_picked'];
     $stage = $dr['matches'];
     $ratios[] = $stage/$total;
   }
