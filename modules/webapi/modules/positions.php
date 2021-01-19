080c652b8d37659d4554d9be2150e54e5ab2c4e1
Eli Silakov <me@ileamare.ru>
2021-01-20 02:13:20 +0300

    webapi: add position *.0 automatic generation (combined position)

--- modules/webapi/modules/positions.php
+++ modules/webapi/modules/positions.php
@@ -27,7 +27,37 @@ $endpoints['positions'] = function($mods, $vars, &$report) {
 
   if (isset($vars['position'])) {
     $position = explode(".", $vars['position']);
-    $res = $context[ (int)$position[0] ][ (int)$position[1] ];
+    if ($position[1] == 0) {
+      //$position[1] = array_keys($context[ (int)$position[0] ])[0];
+      $p0 = [];
+      $p0m = [];
+      foreach($context[ (int)$position[0] ] as $p) {
+        foreach($p as $hero => $stats) {
+          if(!isset($p0[$hero])) {
+            $p0[$hero] = [];
+            $p0m[$hero] = 0;
+          }
+          $p0m[$hero] += $stats['matches_s'];
+          $m = $stats['matches_s'];
+          unset($stats['matches_s']);
+          foreach ($stats as $k => $v) {
+            $p0[$hero][$k] = ($p0[$hero][$k] ?? 0) + $v*$m;
+          }
+        }
+      }
+      foreach($p0 as $hero => $stats) {
+        foreach ($stats as $k => $v) {
+          $p0[$hero][$k] = round($v/$p0m[$hero], 4);
+        }
+        $p0[$hero]['matches_s'] = $p0m[$hero];
+      }
+      $res = $p0;
+      //$res = $context[ (int)$position[0] ][ (int)$position[1] ];
+    } else {
+      if (!isset($context[ (int)$position[0] ][ (int)$position[1] ]))
+        $position[1] = array_keys($context[ (int)$position[0] ])[0];
+      $res = $context[ (int)$position[0] ][ (int)$position[1] ];
+    }
     positions_ranking($res, $context_total_matches);
     
     foreach ($res as $id => $data) {
@@ -35,7 +65,7 @@ $endpoints['positions'] = function($mods, $vars, &$report) {
     }
 
     return [
-      $vars['position'] => $res
+      implode('.', $position) => $res
     ];
   }
   
@@ -44,11 +74,12 @@ $endpoints['positions'] = function($mods, $vars, &$report) {
   $res['total'] = [];
   for ($i=1; $i>=0; $i--) {
     foreach ($context[$i] as $j => &$pos_summary) {
+      if (empty($pos_summary)) continue;
       positions_ranking($pos_summary, $context_total_matches);
       foreach ($pos_summary as $id => $data) {
         if (isset($res['total'][$id])) $res['total'][$id] += $data['matches_s'];
         else $res['total'][$id] = $data['matches_s'];
-        $pos_summary[$id]['picks_to_median'] = isset($median_picks) ? round($el['matches_s']/$median_picks, 3) : null;
+        $pos_summary[$id]['picks_to_median'] = isset($median_picks) ? round($data['matches_s']/$median_picks, 3) : null;
       }
       $res["$i.$j"] = $pos_summary;
     }
@@ -76,7 +107,7 @@ function positions_ranking(&$context, $total_matches) {
         $i++;
         $context[$id]['rank'] = $last_rank;
       } else {
-        $context[$id]['rank'] = 100 - $increment*$i++;
+        $context[$id]['rank'] = round(100 - $increment*$i++, 2);
       }
       $last = $el;
       $last_rank = $context[$id]['rank'];
