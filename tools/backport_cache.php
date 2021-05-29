f1415c4608395d9686b398c04a5dd6212ef29764
Eli Silakov <me@ileamare.ru>
2021-05-29 19:28:23 +0300

    add charset to backport cache

--- tools/backport_cache.php
+++ tools/backport_cache.php
@@ -6,6 +6,8 @@ $conn = new mysqli($lrg_sql_host, $lrg_sql_user, $lrg_sql_pass, $lrg_sql_db);
 
 if ($conn->connect_error) die("[F] Connection to SQL server failed: ".$conn->connect_error."\n");
 
+$conn->set_charset("utf8");
+
 $skip = isset($options['s']);
 
 if(isset($options['c'])) {
