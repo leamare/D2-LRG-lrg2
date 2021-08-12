c12fcb945bbb8800bbf69a69946e229a9692c20c
Eli Silakov <me@ileamare.ru>
2021-08-12 20:49:45 +0300

    wv: add matches to TVT grid

--- modules/view/generators/tvt_grid.php
+++ modules/view/generators/tvt_grid.php
@@ -25,17 +25,25 @@ function rg_generator_tvt_grid($table_id, &$context, $teams_interest = []) {
       } else if($teamline[$team_ids[$i]]['matches'] == 0) {
         $res .= "<td>-</td>";
       } else {
+        $alert = locale_string("matches").": ".$context[$tid][$team_ids[$i]]['matches']
+        ."<br />".locale_string("winrate").": ".number_format($context[$tid][$team_ids[$i]]['winrate'] * 100, 2)
+        ."%<br />".locale_string("won")." ".$context[$tid][$team_ids[$i]]['won']." - "
+          .locale_string("lost")." ".$context[$tid][$team_ids[$i]]['lost'];
+
+        if (isset($context[$tid][$team_ids[$i]]['matchids'])) {
+          $alert .= "<br />".locale_string("matches").": <br />";
+          
+          $sz = $context[$tid][$team_ids[$i]]['matches'] - 1;
+          foreach($context[$tid][$team_ids[$i]]['matchids'] as $n => $mid) {
+            $alert .= match_link($mid).($n < $sz ? ", " : "");
+          }
+        }
+        
         $res .= "<td".
                 ($teamline[$team_ids[$i]]['winrate'] > 0.55 ? " class=\"high-wr\"" : (
                       $teamline[$team_ids[$i]]['winrate'] < 0.45 ? " class=\"low-wr\"" : ""
                     )
-                  )." onclick=\"showModal('".locale_string("matches").": ".$context[$tid][$team_ids[$i]]['matches']
-                        ."<br />".locale_string("winrate").": ".number_format($context[$tid][$team_ids[$i]]['winrate']*100,2)
-                        ."%<br />".locale_string("won")." ".$context[$tid][$team_ids[$i]]['won']." - "
-                                 .locale_string("lost")." ".$context[$tid][$team_ids[$i]]['lost'].(
-                                   isset($context[$tid][$team_ids[$i]]['matchids']) ?
-                                    "<br />MatchIDs: ".implode($context[$tid][$team_ids[$i]]['matchids'], ", ")
-                                    : "").
+                  )." onclick=\"showModal('".addcslashes(htmlspecialchars($alert), "'").
                         "','".addcslashes(team_name($tid)." ".locale_string("vs")." ".team_name($team_ids[$i]), "'")."')\">".
                         number_format($teamline[$team_ids[$i]]['winrate']*100,0)."</td>";
       }
--- modules/view/teams/grid.php
+++ modules/view/teams/grid.php
@@ -8,6 +8,20 @@ function rg_view_generate_teams_grid() {
   $team_ids = array_keys($report['teams']);
   $tvt = rg_generator_tvt_unwrap_data($report['tvt'], $report['teams']);
 
+  if (isset($report['match_participants_teams'])) {
+    foreach ($report['match_participants_teams'] as $mid => $teams) {
+      if (!isset($tvt[$teams['dire']][$teams['radiant']]['matchids'])) {
+        $tvt[$teams['dire']][$teams['radiant']]['matchids'] = [];
+      }
+      $tvt[$teams['dire']][$teams['radiant']]['matchids'][] = $mid;
+
+      if (!isset($tvt[$teams['radiant']][$teams['dire']]['matchids'])) {
+        $tvt[$teams['radiant']][$teams['dire']]['matchids'] = [];
+      }
+      $tvt[$teams['radiant']][$teams['dire']]['matchids'][] = $mid;
+    }
+  }
+
   $res  = "<div class=\"content-text\">".locale_string("desc_tvt")."</div>";
 
   $res .= rg_generator_tvt_grid("teams-tvt", $tvt, $report['teams_interest'] ?? []);
