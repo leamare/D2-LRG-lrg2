79d3a0bc5b44918301243bb80f7adf1640a98472
Eli Silakov <me@ileamare.ru>
2021-09-09 00:30:57 +0300

    add fallback locale support

--- modules/commons/locale_strings.php
+++ modules/commons/locale_strings.php
@@ -5,6 +5,10 @@ function locale_string($string_id, $vars = []) {
   global $locale;
   global $fallback_locale;
 
+  if(empty($fallback_locale) && isset($locale) && isset($strings[$locale]['__fallback'])) {
+    $fallback_locale = $strings[$locale]['__fallback'];
+  }
+
   if(isset($locale) && isset($strings[$locale][$string_id])) {
     $string = $strings[$locale][$string_id];
   } else if (isset($locale) && isset($fallback_locale) && isset($strings[$fallback_locale][$string_id])) {
@@ -18,4 +22,16 @@ function locale_string($string_id, $vars = []) {
   }
 
   return $string;
+}
+
+function include_locale($locale) {
+  global $strings;
+
+  if (file_exists('locales/'.$locale.'.php')) {
+    require_once('locales/'.$locale.'.php');
+  } elseif (file_exists('locales/'.$locale.'.json')) {
+    $strings[$locale] = json_decode(file_get_contents('locales/'.$locale.'.json'), true);
+  } else return false;
+
+  return true;
 }
