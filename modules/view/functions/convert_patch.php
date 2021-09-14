5ef8017cd1fc056cb5306f26e3469db5b5e64bb6
Eli Silakov <me@ileamare.ru>
2021-09-14 04:33:00 +0300

    add unconvert patch

--- modules/view/functions/convert_patch.php
+++ modules/view/functions/convert_patch.php
@@ -29,3 +29,41 @@ function convert_patch($pid) {
     $patch_lo ? chr( ord('a') + $patch_lo ) : ""
   );
 }
+
+function unconvert_patch($patchstr) {
+  global $meta;
+
+  $meta['versions'];
+  $verchars = ['.', '/'];
+  for ($i = 0; $i < 10; $i++) {
+    $verchars[] = "$i";
+  }
+
+  $len = strlen($patchstr);
+  $letter = $patchstr[$len-1];
+  if (in_array($letter, $verchars)) {
+    $letter = null;
+  } else {
+    $patchstr = substr($patchstr, 0, $len-1);
+  }
+
+  $lastpid = 0; $diff = 0;
+  $patchblocks = explode('.', $patchstr, 2);
+  if (strpos($patchblocks[1], '/') !== false) {
+    $patchblocks[1] = explode('/', $patchblocks[1])[0];
+  }
+
+  foreach ($meta['versions'] as $pid => $str) {
+    $ps = explode('.', $str, 2);
+    if (strpos($ps[1], '/') !== false) {
+      $ps[1] = explode('/', $ps[1])[0];
+    }
+    if ($ps[0] != $patchblocks[0]) continue;
+    if ($ps[1] > $patchblocks[1]) break;
+
+    $lastpid = $pid;
+    $diff = $patchblocks[1] - $ps[1];
+  }
+
+  return ($lastpid + $diff) * 100 + ($letter ? ord($letter)-ord('a') : 0);
+}
