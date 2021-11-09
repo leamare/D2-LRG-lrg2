67d98e5e54863f60a603f8c9e948be2b2ee33a4f
Eli Silakov <me@ileamare.ru>
2021-11-09 21:19:53 +0300

    wv: fix finished date in match card

--- modules/view/functions/match_card.php
+++ modules/view/functions/match_card.php
@@ -97,7 +97,7 @@ function match_card($mid) {
                   <div class=\"match-info-line\"><span class=\"caption\">".locale_string("winner").":</span> ".
                     ($report['matches_additional'][$mid]['radiant_win'] ? $team_radiant : $team_dire)."</div>
                   <div class=\"match-info-line\"><span class=\"caption\">".locale_string("date").":</span> ".
-                    date(locale_string("time_format")." ".locale_string("date_format"), $report['matches_additional'][$mid]['date'])."</div>
+                    date(locale_string("time_format")." ".locale_string("date_format"), $report['matches_additional'][$mid]['date'] + $report['matches_additional'][$mid]['duration'])."</div>
               </div>";
 
   return $output."</div>";
