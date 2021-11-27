b43212299ce3c4b73413b688b78f45f7c7c8b145
Eli Silakov <me@ileamare.ru>
2021-11-27 16:34:19 +0300

    add mb_str_split polyfill

--- modules/commons/generate_tag.php
+++ modules/commons/generate_tag.php
@@ -1,36 +1,86 @@
 <?php
 /** 
- * League Report Generator - Generate Team Tag - Fetcher Module
- * ported from D2-LRG-lrg2 v 2.*
- * @author Darien "leamare" Fawkes
- * @license GNU GPL 3.0
- * 
- * @param string $name team's name
- * @return string team tag, based on team's name
+ * Generating team tag
  */
 
-function mb_ucfirst($string, $encoding = "UTF-8") {
+if (!function_exists('mb_ucfirst')) {
+  function mb_ucfirst($string, $encoding = "UTF-8") {
     $firstChar = mb_substr($string, 0, 1, $encoding);
     $then = mb_substr($string, 1, null, $encoding);
     return mb_strtoupper($firstChar, $encoding) . $then;
+  }
 }
 
-function mb_ctype_upper($string) {
-  // return $string == mb_strtoupper($string, $encoding = "UTF-8");
-  return preg_match('~^\p{Lu}~u', $string);
+if (!function_exists('mb_ctype_upper')) {
+  function mb_ctype_upper($string) {
+    // return $string == mb_strtoupper($string, $encoding = "UTF-8");
+    return preg_match('~^\p{Lu}~u', $string);
+  }
 }
 
-function mb_ucwords($string, $delimiters = array(" ", "-", ".", "'", "O'", "Mc")) {
+if (!function_exists('mb_ucwords')) {
+  function mb_ucwords($string, $delimiters = array(" ", "-", ".", "'", "O'", "Mc")) {
     $string = mb_convert_case($string, MB_CASE_TITLE, "UTF-8");
     foreach ($delimiters as $dlnr => $delimiter) {
-        $words = explode($delimiter, $string);
-        $newwords = [];
-        foreach ($words as $wordnr => $word) {
-          $newwords[] = mb_ucfirst($word);
-        }
-        $string = implode($delimiter, $newwords);
+      $words = explode($delimiter, $string);
+      $newwords = [];
+      foreach ($words as $wordnr => $word) {
+        $newwords[] = mb_ucfirst($word);
+      }
+      $string = implode($delimiter, $newwords);
     }
     return $string;
+  }
+}
+
+// Polyfill PHP < 7.4 based on package "symfony/polyfill-mbstring"
+if (!function_exists('mb_str_split')) {
+  function mb_str_split($string, $split_length = 1, $encoding = null) {
+    if (null !== $string && !\is_scalar($string) && !(\is_object($string) && \method_exists($string, '__toString'))) {
+      trigger_error('mb_str_split(): expects parameter 1 to be string, '.\gettype($string).' given', E_USER_WARNING);
+      return null;
+    }
+    if (null !== $split_length && !\is_bool($split_length) && !\is_numeric($split_length)) {
+      trigger_error('mb_str_split(): expects parameter 2 to be int, '.\gettype($split_length).' given', E_USER_WARNING);
+      return null;
+    }
+    $split_length = (int) $split_length;
+    if (1 > $split_length) {
+      trigger_error('mb_str_split(): The length of each segment must be greater than zero', E_USER_WARNING);
+      return false;
+    }
+    if (null === $encoding) {
+      $encoding = mb_internal_encoding();
+    } else {
+      $encoding = (string) $encoding;
+    }
+    
+    if (! in_array($encoding, mb_list_encodings(), true)) {
+      static $aliases;
+      if ($aliases === null) {
+        $aliases = [];
+        foreach (mb_list_encodings() as $encoding) {
+          $encoding_aliases = mb_encoding_aliases($encoding);
+          if ($encoding_aliases) {
+            foreach ($encoding_aliases as $alias) {
+              $aliases[] = $alias;
+            }
+          }
+        }
+      }
+      if (! in_array($encoding, $aliases, true)) {
+        trigger_error('mb_str_split(): Unknown encoding "'.$encoding.'"', E_USER_WARNING);
+        return null;
+      }
+    }
+    
+    $result = [];
+    $length = mb_strlen($string, $encoding);
+    for ($i = 0; $i < $length; $i += $split_length) {
+      $result[] = mb_substr($string, $i, $split_length, $encoding);
+    }
+    return $result;
+  }
 }
 
 function generate_tag($name) {
@@ -67,4 +117,3 @@ function generate_tag($name) {
   return $tag;
 }
 
-?>
