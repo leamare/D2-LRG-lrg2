6411dc02411c7ed6cd7e7713eebb0c0ff21f3d8b
Eli Silakov <me@ileamare.ru>
2022-01-28 20:01:03 +0300

    wv: add custom selectors

--- modules/view/functions/join_selectors.php
+++ modules/view/functions/join_selectors.php
@@ -57,8 +57,8 @@ function join_selectors($modules, $level, $parent="") {
     if($mod_type) {
       if (strpos($parent, "profiles") == strlen($parent)-8)
         $modname .= "";
-        # FIXME Remove this block later, using class-based modules with a parameter
-        #       either show child indicator or not
+        // FIXME: Remove this block later, using class-based modules with a parameter
+        //       either show child indicator or not
       else if ($selectors_num < $max_tabs)
         $modname .= " &#9776;";
       else
@@ -79,6 +79,40 @@ function join_selectors($modules, $level, $parent="") {
                             ($first ? " active" : "")."\" onclick=\"switchTab(event, 'module-".$mn."', 'mod-".$level_codes[$level][1]."');\">".locale_string($modname)."</span>";
       }
     } else {
+      $data_aliases = null;
+      $data_icon = null;
+
+      if (strpos($modtag, 'heroid') !== false) {
+        global $meta;
+        $mods = explode('-', $modtag);
+        foreach ($mods as $m) {
+          if (strpos($m, 'heroid') !== false) {
+            $hid = (int)str_replace('heroid', '', $m);
+            break;
+          }
+        }
+        if (isset($hid)) {
+          $data_aliases = ($meta['heroes'][ $hid ]['alt'] ?? "") . " " . ($meta['heroes'][ $hid ]['aliases'] ?? "");
+          $data_icon = hero_icon_link($hid);
+        }
+      }
+
+      if (strpos($modtag, 'team') !== false) {
+        global $meta;
+        $mods = explode('-', $mn);
+        foreach ($mods as $m) {
+          if (strpos($m, 'team') == 0) {
+            $tid = (int)str_replace('team', '', $m);
+            if ($tid) break;
+          }
+        }
+        if (isset($tid)) {
+          global $team_logo_provider;
+          $data_aliases = team_tag($tid);
+          $data_icon = str_replace('%TEAM%', $tid, $team_logo_provider);
+        }
+      }
+
       if($lrg_use_get && $lrg_get_depth > $level) {
 
         if (stripos($mod, $mn) === 0 && (
@@ -87,12 +121,21 @@ function join_selectors($modules, $level, $parent="") {
             ) )
           $selectors[] = "<option selected=\"selected\" value=\"?league=".$leaguetag."&mod=".$mn."&".
           (empty($linkvars) ? "" : "&".$linkvars)
-          ."\">".$modname."</option>";
+          ."\" ".
+          (isset($data_aliases) ? 'data-aliases="'.$data_aliases.'" ' : '').
+          (isset($data_icon) ? 'data-icon="'.$data_icon.'" ' : '').
+          ">".$modname."</option>";
         else
           $selectors[] = "<option".($unset_selector ? "selected=\"selected\"" : "")." value=\"?league=".$leaguetag."&mod=".$mn.(empty($linkvars) ? "" : "&".$linkvars)
-          ."\">".$modname."</option>";
+            ."\" ".
+            (isset($data_aliases) ? 'data-aliases="'.$data_aliases.'" ' : '').
+            (isset($data_icon) ? 'data-icon="'.$data_icon.'" ' : '').
+          ">".$modname."</option>";
       } else {
-        $selectors[] = "<option value=\"module-".$mn."\">".$modname."</option>";
+        $selectors[] = "<option value=\"module-".$mn."\" ".
+          (isset($data_aliases) ? 'data-aliases="'.$data_aliases.'" ' : '').
+          (isset($data_icon) ? 'data-icon="'.$data_icon.'" ' : '').
+        ">".$modname."</option>";
       }
     }
     if(!$mod_islink && ($startline_check_res || !$lrg_use_get || $lrg_get_depth < $level+1 || $unset_selector)) {
@@ -110,13 +153,16 @@ function join_selectors($modules, $level, $parent="") {
   else
   if($lrg_use_get && $lrg_get_depth > $level)
     return "<div class=\"selector-modules".(empty($level_codes[$level][0]) ? "" : "-".$level_codes[$level][0])."\">".
-        "<select onchange=\"select_modules_link(this);\" class=\"select-selectors select-selectors".(empty($level_codes[$level][0]) ? "" : "-".$level_codes[$level][0])."\">".
-        implode("", $selectors)."</select></div>".$out;
+        "<div class=\"custom-selector\">".
+        "<select onchange=\"select_modules_link(this);\" class=\"select-selectors select-selectors".(empty($level_codes[$level][0]) ? "" : "-".$level_codes[$level][0])." \"".
+        "data-placeholder=\"".locale_string('filter_placeholder')."\">".
+        implode("", $selectors)."</select></div></div>".$out;
     else
-    return "<div class=\"selector-modules".(empty($level_codes[$level][0]) ? "" : "-".$level_codes[$level][0])."\">".
+    return "<div class=\" selector-modules".(empty($level_codes[$level][0]) ? "" : "-".$level_codes[$level][0])."\">".
+        "<div class=\"custom-selector\">".
         "<select onchange=\"switchTab(event, this.value, 'mod-".$level_codes[$level][1]."');\" class=\"select-selectors select-selectors".
-        (empty($level_codes[$level][0]) ? "" : "-".$level_codes[$level][0])."\">".
-        implode("", $selectors)."</select></div>".$out;
+        (empty($level_codes[$level][0]) ? "" : "-".$level_codes[$level][0])."\" ".
+        "data-placeholder=\"".locale_string('filter_placeholder')."\">".
+        implode("", $selectors)."</select></div></div>".$out;
 }
 
-?>
--- res/reports.js
+++ res/reports.js
@@ -14,11 +14,130 @@ $(document).ready(function() {
     const table = $(this).attr('data-table-filter-id');
     
     $("#" + table + " tbody tr").filter(function() {
-      const aliases = $(this).find('img[data-aliases], a[data-aliases]').attr('data-aliases');
+      const aliasesRaw = $(this).find('img[data-aliases], a[data-aliases]');
+      let aliases = $(this).attr('data-aliases');
+      if (aliasesRaw.length) {
+        for (let i=0; i<aliasesRaw.length; i++) {
+          aliases += ' ' + $(aliasesRaw[i]).attr('data-aliases');
+        }
+      }
       const line = $(this).text().toLowerCase() + (aliases ? ' ' + aliases.toLowerCase() : '');
       $(this).toggle( line.indexOf(value) !== -1 )
     });
   });
+
+  let selectors = $('.custom-selector');
+  let ssz = selectors.length;
+
+  for (let si = 0; si < ssz; si++) {
+    let el = selectors[si];
+
+    let select = $(el).find('select');
+    let div = $( '<div class="custom-selector-selected"></div>' );
+    $(div).on('click', function(e) {
+      let isOpen = !$(this).parent().hasClass('select-show');
+      // closeAllSelect($(this).parent());
+      $(this).parent().toggleClass('select-show');
+      if (isOpen) {
+        $(this).siblings('input.search-filter-selector').focus();
+      }
+      e.stopPropagation();
+    });
+
+    const selected = $(select).find('option:selected');
+    const icon = $(selected).attr('data-icon');
+    const selectedValue = selected.html();
+    
+    $(div).html( selectedValue );
+    if (icon) {
+      $(div).prepend( $('<img>', { src: icon, class: 'custom-selector-option-icon' }) );
+    }
+    $(el).append(div);
+
+    div = $( '<div class="custom-selector-values custom-selector-list"></div>' );
+    let options = select.children();
+    let sz = options.length;
+
+    for(let i=0; i<sz; i++) {
+      const option = $( '<div class="custom-selector-option"></div>' );
+      const icon = $(options[i]).attr('data-icon');
+      const value = $(options[i]).html();
+      $(option).html(value);
+      $(option).attr( 'data-aliases', $(options[i]).attr('data-aliases') );
+      $(option).attr( 'data-value', value );
+      if (icon) {
+        $(option).prepend( $('<img>', { src: icon, class: 'custom-selector-option-icon' }) );
+      } else {
+        $(option).addClass('no-icon');
+      }
+      if (selectedValue == value) $(option).addClass('as-selected');
+
+      $(option).on('click', function (e) {
+        const value = $(this).attr('data-value');
+        const select = $(this).parent().parent().find('select')[0];
+        const cselected = $(this).parent().parent().find('.custom-selector-selected')[0];
+        const sl = select.length;
+        let link;
+
+        for (i = 0; i < sl; i++) {
+          if ($(select[i]).html() == value) {
+            select.selectedIndex = i;
+            $(cselected).html(value);
+            let icon = $(options[i]).attr('data-icon');
+            if (icon) {
+              $(cselected).prepend( $('<img>', { src: icon, class: 'custom-selector-option-icon' }) );
+            }
+
+            link = $(select[i]).attr('value');
+
+            let asSelected = $(this).parent('.as-selected');
+            let sz = asSelected.length;
+            for (j = 0; j < sz; j++) {
+              $(asSelected[j]).removeClass('as-selected');
+            }
+            $(this).addClass('as-selected');
+
+            break;
+          }
+        }
+
+        closeAllSelect();
+        e.stopPropagation();
+
+        $(select).trigger('change');
+      });
+
+      $(div).append(option);
+    }
+
+    $(el).append( div );
+
+    if (sz > 10) {
+      const placeholder = $(select).attr('data-placeholder');
+      const input = $(`<input type="text" name="filter" class="search-filter-selector" placeholder="${placeholder}" />`);
+
+      $(input).on('keyup', function() {
+        const value = $(this).val().toLowerCase();
+        const options = $(this).parent().find('.custom-selector-list')[0];
+        
+        $(options).children('.custom-selector-option').filter(function() {
+          const aliases = $(this).attr('data-aliases');
+          const line = $(this).text().toLowerCase() + (aliases ? ' ' + aliases.toLowerCase() : '');
+          $(this).toggle( line.indexOf(value) !== -1 )
+        });
+      });
+
+      $(input).on('click', function(e) {
+        e.stopPropagation();
+      });
+
+      $(input).insertBefore( div );
+
+      $(el).addClass('searchable');
+    }
+  }
+
+  $(document).on('click', closeAllSelect);
 });
 
 
@@ -46,6 +165,15 @@ document.getElementById('modal-text').onclick = function(event) {
     }
 }
 
+function closeAllSelect(el) {
+  let selects = $('.custom-selector.select-show');
+  let sz = selects.length;
+  for (let i = 0; i<sz; i++) {
+    if (el == selects[i]) continue;
+    $(selects[i]).removeClass('select-show');
+  }
+}
+
 function switchTab(evt, moduleID, className) {
     var i, tabcontent, tablinks;
     parent = document.getElementById(moduleID).parentNode;
@@ -62,7 +190,9 @@ function switchTab(evt, moduleID, className) {
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(moduleID).className += " active";
-    evt.currentTarget.className += " active";
+    if (evt) {
+      evt.currentTarget.className += " active";
+    }
 }
 
 
@@ -70,8 +200,6 @@ function select_modules_link(a) {
   if(a.value)
     window.location=a.value;
 }
-
-
 
 $(".tagsshow .category").on("click", () => {
     if ($(".tagslist").hasClass("hidden")) {
