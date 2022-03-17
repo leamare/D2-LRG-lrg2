ad9b2c0cfe553d42341bf088250c2fdf467295ae
Eli Silakov <me@ileamare.ru>
2022-03-18 00:20:03 +0300

    wv: add additional matches tabs to teams and regions

--- modules/view/regions/matches.php
+++ modules/view/regions/matches.php
@@ -1,14 +1,56 @@
 <?php
 
-$res["region".$region]["matches"] = "";
+$res["region".$region]["matches"] = [];
+
+include_once($root."/modules/view/generators/matches_list.php");
 
 if(check_module($modstr."-matches")) {
-  $res["region".$region]['matches'] = "<div class=\"content-text\">".locale_string("desc_matches")."</div>";
-  $res["region".$region]['matches'] .= "<div class=\"content-cards\">";
-  foreach($reg_report['matches'] as $matchid => $match) {
-    $res["region".$region]['matches'] .= match_card($matchid);
-  }
-  $res["region".$region]['matches'] .= "</div>";
-}
+  if($mod == $modstr."-matches") $unset_module = true;
+  $reslocal = [];
+  $parent = $modstr."-matches-";
 
-?>
+  $reslocal['list'] = "";
+  if (check_module($parent."list")) {
+    $reslocal['list'] = rg_generator_matches_list("matches-list-region$region", $reg_report['matches']);
+  }
+
+  $reslocal['cards'] = "";
+  if (check_module($parent."cards")) {
+    krsort($reg_report['matches']);
+    $reslocal['cards'] = "<div class=\"content-text\">".locale_string("desc_matches")."</div>";
+    $reslocal['cards'] .= "<div class=\"content-cards\">";
+    foreach($reg_report['matches'] as $matchid => $match) {
+      $reslocal['cards'] .= match_card($matchid);
+    }
+    $reslocal['cards'] .= "</div>";
+  }
+
+  $reslocal['heroes'] = [];
+  if (check_module($parent."heroes")) {
+    if ($mod == $parent."heroes") $unset_module = true;
+    $parent .= "heroes-";
+
+    global $meta, $strings;
+
+    $hnames = [];
+    foreach ($meta['heroes'] as $id => $v) {
+      $hnames[$id] = $v['name'];
+      $strings['en']["heroid".$id] = $v['name'];
+    }
+  
+    uasort($hnames, function($a, $b) {
+      if($a == $b) return 0;
+      else return ($a > $b) ? 1 : -1;
+    });
+  
+    foreach($hnames as $hid => $name) {
+      $reslocal['heroes']["heroid".$hid] = "";
+  
+      if(check_module($parent."heroid".$hid)) {
+        $reslocal['heroes']["heroid".$hid] = rg_generator_hero_matches_list("matches-region$region-heroes-$hid", $hid, null, true, $reg_report['matches']);
+      }
+    }
+  }
+  
+  $res["region".$region]['matches'] = $reslocal;
+}
--- modules/view/teams/profiles.php
+++ modules/view/teams/profiles.php
@@ -601,18 +601,64 @@ function rg_view_generate_teams_profiles($context, $context_mod, $foreword = "")
 
 
       if (isset($context[$tid]['matches']) && isset($report['matches'])) {
-        $res["team".$tid]['matches'] = "";
+        $res["team".$tid]['matches'] = [];
+
+        include_once($root."/modules/view/generators/matches_list.php");
 
         if(check_module($context_mod."team".$tid."-matches")) {
-          $res["team".$tid]['matches'] = "<div class=\"content-text\">".locale_string("desc_matches")."</div>";
-          $res["team".$tid]['matches'] .= "<div class=\"content-cards\">";
-          krsort($context[$tid]['matches']);
-          foreach($context[$tid]['matches'] as $matchid => $match) {
-            $res["team".$tid]['matches'] .= match_card($matchid);
+          if($mod == $context_mod."team".$tid."-matches") $unset_module = true;
+          $reslocal = [];
+          $parent = $context_mod."team".$tid."-matches-";
+
+          $reslocal['list'] = "";
+          if (check_module($parent."list")) {
+            $reslocal['list'] = rg_generator_matches_list("matches-list-team$tid", $context[$tid]['matches']);
           }
-          $res["team".$tid]['matches'] .= "</div>";
+
+          $reslocal['cards'] = "";
+          if (check_module($parent."cards")) {
+            krsort($context[$tid]['matches']);
+            $reslocal['cards'] = "<div class=\"content-text\">".locale_string("desc_matches")."</div>";
+            $reslocal['cards'] .= "<div class=\"content-cards\">";
+            foreach($context[$tid]['matches'] as $matchid => $match) {
+              $reslocal['cards'] .= match_card($matchid);
+            }
+            $reslocal['cards'] .= "</div>";
+          }
+
+          $reslocal['heroes'] = [];
+          if (check_module($parent."heroes")) {
+            if ($mod == $parent."heroes") $unset_module = true;
+            $parent .= "heroes-";
+  
+            global $meta, $strings;
+  
+            $hnames = [];
+            foreach ($meta['heroes'] as $id => $v) {
+              $hnames[$id] = $v['name'];
+              $strings['en']["heroid".$id] = $v['name'];
+            }
+          
+            uasort($hnames, function($a, $b) {
+              if($a == $b) return 0;
+              else return ($a > $b) ? 1 : -1;
+            });
+          
+            foreach($hnames as $hid => $name) {
+              $reslocal['heroes']["heroid".$hid] = "";
+          
+              if(check_module($parent."heroid".$hid)) {
+                $reslocal['heroes']["heroid".$hid] = rg_generator_hero_matches_list(
+                  "matches-team$tid-heroes-$hid", $hid, null, true, $context[$tid]['matches']
+                );
+              }
+            }
+          }
+
+          $res["team".$tid]['matches'] = $reslocal;
         }
       }
+      
       if (isset($report['players'])) {
         $res["team".$tid]['roster'] = "";
 
