9e1ca9a8babaf6dd78900b962da934ee03e209d3
Lea Silakov <me@ileamare.ru>
2022-12-26 00:46:52 +0300

    wv: add support for players-limiter

--- modules/view/players/haverages.php
+++ modules/view/players/haverages.php
@@ -8,7 +8,7 @@ function rg_view_generate_players_haverages() {
   global $report;
 
   $res = "<div class=\"content-text\">".
-          locale_string("desc_players_avg", ["lim" => $report['settings']['limiter_triplets']+1 ]).
+          locale_string("desc_players_avg", ["lim" => ($report['settings']['limiter_players'] ?? $report['settings']['limiter_triplets'])+1 ]).
           "</div>";
 
   $res .= rg_generator_haverages("players-havgs", $report['averages_players'], false);
