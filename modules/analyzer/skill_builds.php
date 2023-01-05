154b59561ee8448c749a777450ad653fcad2ff37
Lea Silakov <me@ileamare.ru>
2023-01-05 15:49:24 +0300

    ana: add skill builds dummy (runtime fix)

---
+++ modules/analyzer/skill_builds.php
@@ -0,0 +1,24 @@
+<?php 
+
+// "matchid": "6931067185",
+// "playerid": 86785083,
+// "hero_id": 60,
+// "skill_build": "[5275,5275,5277,5277,5277,5276,5278,5277,5276,816,5276,5278,5276,5275,480,5275,5278,6080]",
+// "first_point_at": "{\\\"5275\\\":1,\\\"5277\\\":3,\\\"5276\\\":6,\\\"5278\\\":7}",
+// "maxed_at": "{\\\"5277\\\":8,\\\"5276\\\":13,\\\"5275\\\":16,\\\"5278\\\":18}",
+// "priority": "{\\\"5278\\\":1,\\\"5277\\\":2,\\\"5276\\\":3,\\\"5275\\\":4}",
+// "talents": "[816,480,6080]",
+// "attributes": "{\\\"count\\\":0,\\\"firstPointAt\\\":false}",
+// "ultimate": 5278
+
+// group by priority
+// priority stats
+// priority frequency by spell
+// talents + skilled
+
+// starting 
+// group by full build
+// individual items
+
+// group by maxedAt
+// group by first skilled at
