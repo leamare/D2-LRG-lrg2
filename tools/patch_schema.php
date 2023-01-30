25ba877ad28d2a1a22e57684ac207946d3b85281
Lea Silakov <me@ileamare.ru>
2023-01-30 19:50:00 +0300

    fet: wards fixes

--- tools/patch_schema.php
+++ tools/patch_schema.php
@@ -103,7 +103,7 @@ if (!$schema['wards']) {
     UNIQUE KEY `wards_matchid_player_IDX` (`matchid`,`playerid`) USING BTREE,
     KEY `wards_matchid_IDX` (`matchid`) USING BTREE,
     KEY `wards_playerid_IDX` (`playerid`) USING BTREE,
-    KEY `wards_heroid_IDX` (`hero_id`) USING BTREE,
+    KEY `wards_heroid_IDX` (`hero_id`) USING BTREE
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
 
   $schema['skill_build_attr'] = true;
