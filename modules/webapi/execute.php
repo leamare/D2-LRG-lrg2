710ec7c6c88a77d8550a752f4198d7cc61246c52
Lea Silakov <me@ileamare.ru>
2023-02-27 20:12:07 +0300

    wa: update execution callback

--- modules/webapi/execute.php
+++ modules/webapi/execute.php
@@ -1,12 +1,24 @@
 <?php 
 
 function execute(&$modline, &$endp, &$vars, &$report) {
-  global $resp;
+  global $resp, $__lrg_onerror;
 
   try {
     $result = $endp($modline, $vars, $report);
     return $result;
   } catch (\Exception $e) {
+    if (!empty($__lrg_onerror)) {
+      $__lrg_onerror([
+        'type' => 'error',
+        'project' => $projectName ?? "LRG2",
+        'path' => $_SERVER['REQUEST_URI'] ?? null,
+        'message' => $e->getMessage()."::".json_encode($e->getTrace()),
+        'file' => str_replace(__DIR__, "", $e->getFile()),
+        'line' => $e->getLine(),
+        'severity' => E_ERROR | $e->getCode(),
+      ]);
+    }
+
     if (!isset($resp['errors'])) $resp['errors'] = [];
       $resp['errors'][] = $e->getMessage().' ('.$e->getFile().':'.$e->getLine().')';
   }
