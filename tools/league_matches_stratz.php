0412ce8954ca0107d7020ceffe0f479085c22c02
Lea Silakov <me@ileamare.ru>
2023-06-12 04:24:14 +0300

    add league matches request tool (using stratz)

---
+++ tools/league_matches_stratz.php
@@ -0,0 +1,64 @@
+<?php
+require_once('head.php');
+
+$_file = !empty($options['o']) ? $options['o'] : "matchlists/$lrg_league_tag.list";
+
+$_ticket = !empty($options['T']) ? $options['T'] : die("# Specify ticket ID\n");
+
+$matches = [];
+$i = 0;
+
+while (true) {
+  $data = [
+    'query' => "query TicketMatches {
+      league(id: {$_ticket}) {
+        matches(request: {take: 100, skip: ".($i * 100)."}) {
+          id
+        }
+      }
+    }"
+  ];
+
+  $data['query'] = str_replace("  ", "", $data['query']);
+  $data['query'] = str_replace("\n", " ", $data['query']);
+
+  if (!empty($stratztoken)) $data['key'] = $stratztoken;
+    
+  $stratz_request = "https://api.stratz.com/graphql";
+
+  $q = http_build_query($data);
+
+  $json = file_get_contents($stratz_request.'?'.$q, false, stream_context_create([
+    'ssl' => [
+      'verify_peer' => false,
+      'verify_peer_name' => false,
+    ]
+  ]));
+
+  sleep(1);
+
+  $stratz = json_decode($json, true);
+
+  echo ".";
+
+  if (!empty($stratz['errors'])) {
+    throw new Exception(json_encode($stratz['errors'], JSON_PRETTY_PRINT));
+  }
+
+  if (empty($stratz['data']) || !empty($stratz['errors'])) {
+    return null;
+  }
+
+  if (empty($stratz['data']['league']['matches'])) break;
+
+  foreach ($stratz['data']['league']['matches'] as $match) {
+    $matches[] = $match['id'];
+  }
+
+  $i++;
+}
+
+$matches = implode("\n", $matches);
+echo "\n";
+
+file_put_contents($_file, $matches);
