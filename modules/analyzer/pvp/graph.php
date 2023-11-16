ff65fcea1e39d291fcc9272bb19d0fe8b34e346a
Lea Silakov <me@ileamare.ru>
2023-11-16 21:39:16 +0300

    ana: adjust players graph to use higher limiter instead of graph limiter (GL is too high)

--- modules/analyzer/pvp/graph.php
+++ modules/analyzer/pvp/graph.php
@@ -3,7 +3,10 @@ $result["players_combo_graph"] = rg_query_player_graph(
   $conn,
   $result['players_summary'],
   $result['random']['matches_total'],
-  $limiters_players['limiter_graph'],
+  $limiters_players['limiter_higher'], // has limiter_graph parameter, but it's way too low
   null
 );
+
+// would be nice to use pairs limiters like for heroes
+// but it's probably out of the picture so we are left with estimates instead
 
