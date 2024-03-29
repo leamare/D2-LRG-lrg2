c826fc0645109df7d034c3600566b021a91fd12b
Lea Silakov <me@ileamare.ru>
2024-03-29 22:44:28 +0300

    wv: add sti components to players

--- modules/view/players.php
+++ modules/view/players.php
@@ -23,64 +23,96 @@ if (isset($report['player_pairs']) || isset($report['player_triplets']) || isset
 if (isset($report['players_combo_graph']) && $report['settings']['players_combo_graph'] && isset($report['players_additional']))
   include("players/party_graph.php");
 
+if (isset($report['starting_items_players']) ) {
+  $modules['players']['items'] = [];
+
+  if (isset($report['starting_items_players']['items'])) {
+    include("players/starting_items.php");
+  }
+  if (isset($report['starting_items_players']['builds'])) {
+    include("players/starting_builds.php");
+  }
+  if (isset($report['starting_items_players']['consumables'])) {
+    include("players/consumables.php");
+  }
+}
+
 if (isset($report['players_summary']) ) {
   include("players/summary.php");
   include("players/profiles.php");
 }
 
 
-  function rg_view_generate_players() {
-    global $report, $mod, $parent, $unset_module;
+function rg_view_generate_players() {
+  global $report, $mod, $parent, $unset_module, $carryon;
 
-    if($mod == "players") $unset_module = true;
-    $parent = "players-";
-    $res = [];
+  if($mod == "players") $unset_module = true;
+  $parent = "players-";
+  $res = [];
 
-    if (isset($report['averages_players'])) {
-      if (check_module($parent."haverages")) {
-        $res['haverages'] = rg_view_generate_players_haverages();
-      }
+  if (isset($report['averages_players'])) {
+    if (check_module($parent."haverages")) {
+      $res['haverages'] = rg_view_generate_players_haverages();
     }
-    if (isset($report['players_draft'])) {
-      if (check_module($parent."draft")) {
-        $res['draft'] = rg_view_generate_players_draft();
-      }
+  }
+  if (isset($report['players_draft'])) {
+    if (check_module($parent."draft")) {
+      $res['draft'] = rg_view_generate_players_draft();
     }
-    if (isset($report['player_positions'])) {
-      if(check_module($parent."positions")) {
-        $res['positions'] = rg_view_generate_players_positions();
-      }
+  }
+  if (isset($report['player_positions'])) {
+    if(check_module($parent."positions")) {
+      $res['positions'] = rg_view_generate_players_positions();
     }
-    if (isset($report['player_laning'])) {
-      if(check_module($parent."laning")) {
-        $res['laning'] = rg_view_generate_players_laning();
-      }
+  }
+  if (isset($report['player_laning'])) {
+    if(check_module($parent."laning")) {
+      $res['laning'] = rg_view_generate_players_laning();
     }
-    if (isset($report['pvp'])) {
-      if (check_module($parent."pvp")) {
-        $res['pvp'] = rg_view_generate_players_pvp();
-      }
+  }
+  if (isset($report['pvp'])) {
+    if (check_module($parent."pvp")) {
+      $res['pvp'] = rg_view_generate_players_pvp();
     }
-    if (isset($report['player_pairs']) || isset($report['player_triplets']) || isset($report['player_lane_combos'])) {
-      if (check_module($parent."combos")) {
-        $res['combos'] = rg_view_generate_players_combos();
-      }
+  }
+  if (isset($report['player_pairs']) || isset($report['player_triplets']) || isset($report['player_lane_combos'])) {
+    if (check_module($parent."combos")) {
+      $res['combos'] = rg_view_generate_players_combos();
     }
-    if (isset($report['players_combo_graph']) && $report['settings']['players_combo_graph'] && isset($report['players_additional'])) {
-      if (check_module($parent."party_graph")) {
-        $res['party_graph'] = rg_view_generate_players_party_graph();
-      }
+  }
+  if (isset($report['players_combo_graph']) && $report['settings']['players_combo_graph'] && isset($report['players_additional'])) {
+    if (check_module($parent."party_graph")) {
+      $res['party_graph'] = rg_view_generate_players_party_graph();
     }
-    if (isset($report['players_summary'])) {
-      if(check_module($parent."summary")) {
-        $res['summary'] = rg_view_generate_players_summary();
+  }
+  if (isset($report['starting_items_players'])) {
+    if (check_module($parent."items")) {
+      if($mod == $parent."items") {
+        $unset_module = true;
       }
-      if(check_module($parent."profiles")) {
-        $res['profiles'] = rg_view_generate_players_profiles();
-      }
-    }
+      $parent .= "items-";
 
-    return $res;
+      $carryon["/^players-items-(stitems|stibuilds|sticonsumables)$/"] = "/^players-items-(stitems|stibuilds|sticonsumables)/";
+
+      if(isset($report['starting_items_players']['items']) && check_module($parent."stitems")) {
+        $res['items']['stitems'] = rg_view_generate_players_sti_items();
+      }
+      if(isset($report['starting_items_players']['builds']) && check_module($parent."stibuilds")) {
+        $res['items']['stibuilds'] = rg_view_generate_players_sti_builds();
+      }
+      if(isset($report['starting_items_players']['consumables']) && check_module($parent."sticonsumables")) {
+        $res['items']['sticonsumables'] = rg_view_generate_players_sti_consumables();
+      }
+    }
+  }
+  if (isset($report['players_summary'])) {
+    if(check_module($parent."summary")) {
+      $res['summary'] = rg_view_generate_players_summary();
+    }
+    if(check_module($parent."profiles")) {
+      $res['profiles'] = rg_view_generate_players_profiles();
+    }
   }
 
-?>
+  return $res;
+}
