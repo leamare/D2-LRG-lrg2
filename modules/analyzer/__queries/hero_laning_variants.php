8338507f6e510e34df85c52101fd46fb05db13aa
Lea Silakov <me@ileamare.ru>
2024-07-08 23:51:03 +0300

    ana: hero laning variants query

---
+++ modules/analyzer/__queries/hero_laning_variants.php
@@ -0,0 +1,193 @@
+<?php 
+
+function rg_query_hero_laning_variants(&$conn, $cluster = null, $team = null, $players = null) {
+  global $players_interest;
+  if (empty($players) && !empty($players_interest)) {
+    $players = $players_interest;
+  }
+
+  $tie_factor = 0.075;
+
+  $result = [ [[]] ];
+  //echo "[S] Requested data for HERO DRAFT\n";
+
+  $sql = "SELECT
+    ams_heroid hero,
+    ams_variant variant,
+    SUM(1) matches,
+    SUM((ams_efficiency_at10 - amr_efficiency_at10) >= $tie_factor) lanes_won,
+    SUM(ABS(ams_efficiency_at10 - amr_efficiency_at10) < $tie_factor) lanes_tied,
+    SUM((ams_efficiency_at10 - amr_efficiency_at10) <= -$tie_factor) lanes_lost,
+    SUM(NOT (ams_q.isRadiant XOR radiantWin)) wins,
+    SUM(NOT (ams_q.isRadiant XOR radiantWin))/SUM(1) winrate,
+    SUM( IF(ams_efficiency_at10 - amr_efficiency_at10 > 0, ams_efficiency_at10 - amr_efficiency_at10, 0) )/SUM( IF(ams_efficiency_at10 - amr_efficiency_at10 > 0, 1, 0) ) avg_advantage,
+    SUM( IF(ams_efficiency_at10 - amr_efficiency_at10 < 0, ams_efficiency_at10 - amr_efficiency_at10, 0) )/SUM( IF(ams_efficiency_at10 - amr_efficiency_at10 < 0, 1, 0) ) avg_disadvantage,
+    SUM( (NOT (ams_q.isRadiant XOR radiantWin)) AND ((ams_efficiency_at10 - amr_efficiency_at10) <= -$tie_factor) ) won_from_behind,
+    SUM( (NOT (ams_q.isRadiant XOR radiantWin)) AND (ABS(ams_efficiency_at10 - amr_efficiency_at10) < $tie_factor) ) won_from_tie,
+    SUM( (NOT (ams_q.isRadiant XOR radiantWin)) AND ((ams_efficiency_at10 - amr_efficiency_at10) >= $tie_factor) ) won_from_won,
+    SUM(ams_efficiency_at10 - amr_efficiency_at10)/SUM(1) avg_gold_diff
+  FROM
+  (
+    SELECT
+      ml.matchid,
+      ams.heroid ams_heroid,
+      ml.variant ams_variant,
+      se.core_lane_eff ams_efficiency_at10,
+      ml.isRadiant,
+      se.lane_c lane,
+      ml.playerid
+    FROM adv_matchlines ams
+    JOIN matchlines ml ON ams.matchid = ml.matchid AND ams.playerid = ml.playerid
+    
+    JOIN (
+      SELECT MAX(adv_matchlines.efficiency_at10) core_lane_eff, matchlines.isRadiant isRadiant, matchlines.matchid matchid, IF(adv_matchlines.lane > 3, 3, adv_matchlines.lane) lane_c
+      FROM adv_matchlines JOIN matchlines ON adv_matchlines.matchid = matchlines.matchid AND adv_matchlines.playerid = matchlines.playerid
+      GROUP BY matchlines.matchid, matchlines.isRadiant, lane_c
+    ) se ON ml.matchid = se.matchid AND ml.isRadiant = se.isRadiant AND se.lane_c = IF(ams.lane > 3, 3, ams.lane)
+  
+    GROUP BY ml.matchid, ams_heroid
+  ) ams_q
+  JOIN 
+  (
+      SELECT MAX(adv_matchlines.efficiency_at10) amr_efficiency_at10, matchlines.isRadiant isRadiant, matchlines.matchid matchid, IF(adv_matchlines.lane > 3, 3, adv_matchlines.lane) lane_c
+      FROM adv_matchlines JOIN matchlines ON adv_matchlines.matchid = matchlines.matchid AND adv_matchlines.playerid = matchlines.playerid
+      GROUP BY matchlines.matchid, matchlines.isRadiant, lane_c
+  ) amr_q
+  ON ams_q.matchid = amr_q.matchid AND ams_q.isRadiant <> amr_q.isRadiant AND ams_q.lane = amr_q.lane_c
+  JOIN matches m ON ams_q.matchid = m.matchid ".
+    ($team === null ? "" : " JOIN teams_matches ON teams_matches.matchid = m.matchid AND ml.isRadiant = teams_matches.is_radiant ").
+    ($team !== null || $cluster !== null ? " WHERE " : "").
+    ($cluster === null ? "" : " AND m.cluster IN (".implode(",", $cluster).") ").
+    ($team === null ? "" : " AND teams_matches.teamid = ".$team." ").
+    (!empty($players) ? " AND ams_q.playerid in (".implode(',', $players).")" : "").
+  " GROUP BY hero, variant";
+
+  if ($conn->multi_query($sql) !== TRUE) throw new \Exception($conn->error);
+
+  $query_res = $conn->store_result();
+  for ($row = $query_res->fetch_assoc(); $row != null; $row = $query_res->fetch_assoc()) {
+    $row['avg_advantage'] = round($row['avg_advantage'], 4);
+    $row['avg_disadvantage'] = round($row['avg_disadvantage'], 4);
+    $row['lane_wr'] = round( ( $row['lanes_won']*2+$row['lanes_tied'] )/($row['matches']*2) , 4);
+    // $row['wr_from_behind'] = round( $row['lanes_lost'] ? ($row['won_from_behind']/$row['lanes_lost']) : 0, 4);
+    // $row['wr_from_tie'] = round( $row['lanes_tied'] ? ($row['won_from_tie']/$row['lanes_tied']) : 0, 4);
+    // $row['wr_from_won'] = round( $row['lanes_won'] ? ($row['won_from_won']/$row['lanes_won']) : 0, 4);
+    foreach($row as $k => $v) {
+      if (is_numeric($v)) {
+        $row[$k] = +$v;
+      }
+    }
+
+    $result[0][ $row['hero'].'-'.$row['variant'] ] = $row;
+    $result[ $row['hero'].'-'.$row['variant'] ] = [
+      0 => $row,
+    ];
+  }
+
+  $query_res->free_result();
+
+  $keys = array_keys(reset($result[0]));
+  $result[0][0] = array_flip($keys);
+
+  // foreach ($result['0'] as $hid => $d) {
+  //   echo $hid;
+
+    // $r = [];
+
+    $sql = "SELECT
+      ams_heroid,
+      ams_variant,
+      amr_heroid hero,
+      amr_variant variant,
+      COUNT(DISTINCT ams_q.matchid) matches,
+      SUM((ams_efficiency_at10 - amr_efficiency_at10) >= $tie_factor) lanes_won,
+      SUM(ABS(ams_efficiency_at10 - amr_efficiency_at10) < $tie_factor) lanes_tied,
+      SUM((ams_efficiency_at10 - amr_efficiency_at10) <= -$tie_factor) lanes_lost,
+      SUM(ams_q.isRadiant = radiantWin) wins,
+      SUM(ams_q.isRadiant = radiantWin)/SUM(1) winrate,
+      SUM( IF(ams_efficiency_at10 - amr_efficiency_at10 > 0, ams_efficiency_at10 - amr_efficiency_at10, 0) )/SUM( IF(ams_efficiency_at10 - amr_efficiency_at10 > 0, 1, 0) ) avg_advantage,
+      SUM( IF(ams_efficiency_at10 - amr_efficiency_at10 < 0, ams_efficiency_at10 - amr_efficiency_at10, 0) )/SUM( IF(ams_efficiency_at10 - amr_efficiency_at10 < 0, 1, 0) ) avg_disadvantage,
+      SUM( (ams_q.isRadiant = radiantWin) AND ((ams_efficiency_at10 - amr_efficiency_at10) <= -$tie_factor) ) won_from_behind,
+      SUM( (ams_q.isRadiant = radiantWin) AND (ABS(ams_efficiency_at10 - amr_efficiency_at10) < $tie_factor) ) won_from_tie,
+      SUM( (ams_q.isRadiant = radiantWin) AND ((ams_efficiency_at10 - amr_efficiency_at10) >= $tie_factor) ) won_from_won,
+      SUM(ams_efficiency_at10 - amr_efficiency_at10)/SUM(1) avg_gold_diff
+    FROM
+    (
+      SELECT
+        ml.matchid,
+        ams.heroid ams_heroid,
+        ml.variant ams_variant,
+        se.core_lane_eff ams_efficiency_at10,
+        ml.isRadiant,
+        IF(ams.lane > 3, 3, ams.lane) lane,
+        ml.playerid
+      FROM adv_matchlines ams
+      JOIN matchlines ml ON ams.matchid = ml.matchid AND ams.heroid = ml.heroid
+      
+      JOIN (
+        SELECT MAX(adv_matchlines.efficiency_at10) core_lane_eff, matchlines.isRadiant isRadiant, matchlines.matchid matchid, IF(adv_matchlines.lane > 3, 3, adv_matchlines.lane) lane_c
+        FROM adv_matchlines JOIN matchlines ON adv_matchlines.matchid = matchlines.matchid AND adv_matchlines.heroid = matchlines.heroid
+        GROUP BY matchlines.matchid, matchlines.isRadiant, lane_c
+      ) se ON ml.matchid = se.matchid AND ml.isRadiant = se.isRadiant AND se.lane_c = IF(ams.lane > 3, 3, ams.lane)
+    
+      GROUP BY ml.matchid, ams_heroid
+    ) ams_q
+    JOIN 
+    (
+      SELECT
+        ml.matchid,
+        ams.heroid amr_heroid,
+        ml.variant amr_variant,
+        se.core_lane_eff amr_efficiency_at10,
+        ml.isRadiant,
+        4-IF(ams.lane > 3, 3, ams.lane) lane,
+        ml.playerid
+      FROM adv_matchlines ams
+      JOIN matchlines ml ON ams.matchid = ml.matchid AND ams.heroid = ml.heroid
+      
+      JOIN (
+        SELECT MAX(adv_matchlines.efficiency_at10) core_lane_eff, matchlines.isRadiant isRadiant, matchlines.matchid matchid, IF(adv_matchlines.lane > 3, 3, adv_matchlines.lane) lane_c
+        FROM adv_matchlines JOIN matchlines ON adv_matchlines.matchid = matchlines.matchid AND adv_matchlines.heroid = matchlines.heroid
+        GROUP BY matchlines.matchid, matchlines.isRadiant, lane_c
+      ) se ON ml.matchid = se.matchid AND ml.isRadiant = se.isRadiant AND se.lane_c = IF(ams.lane > 3, 3, ams.lane)
+    
+      GROUP BY ml.matchid, amr_heroid
+    ) amr_q
+    ON ams_q.matchid = amr_q.matchid AND ams_q.isRadiant <> amr_q.isRadiant AND ams_q.lane = amr_q.lane
+    JOIN matches m ON ams_q.matchid = m.matchid ".
+    ($team === null ? "" : " JOIN teams_matches ON teams_matches.matchid = m.matchid AND ml.isRadiant = teams_matches.is_radiant ").
+    ($team !== null || $cluster !== null ? " WHERE " : "").
+    ($cluster === null ? "" : " AND m.cluster IN (".implode(",", $cluster).") ").
+    ($team === null ? "" : " AND teams_matches.teamid = ".$team." ").
+    (!empty($players) ? " AND ( ams_q.playerid in (".implode(',', $players).") and amr_q.playerid in (".implode(',', $players).") )" : "").
+  " GROUP BY ams_heroid, hero, ams_variant, variant";
+
+    if ($conn->multi_query($sql) !== TRUE) throw new \Exception($conn->error);
+
+    $query_res = $conn->store_result();
+    for ($row = $query_res->fetch_assoc(); $row != null; $row = $query_res->fetch_assoc()) {
+      $ams = $row['ams_heroid'].'-'.$row['ams_variant'];
+      unset($row['ams_heroid']); unset($row['ams_variant']);
+
+      if (!isset($result[ $ams ])) {
+        $result[ $ams ] = [ 0 => array_flip($keys) ];
+      }
+
+      $row['avg_advantage'] = round($row['avg_advantage'], 4);
+      $row['avg_disadvantage'] = round($row['avg_disadvantage'], 4);
+      $row['lane_wr'] = round( ( $row['lanes_won']*2+$row['lanes_tied'] )/($row['matches']*2) , 4);
+      // $row['wr_from_behind'] = round( $row['lanes_lost'] ? ($row['won_from_behind']/$row['lanes_lost']) : 0, 4);
+      // $row['wr_from_tie'] = round( $row['lanes_tied'] ? ($row['won_from_tie']/$row['lanes_tied']) : 0, 4);
+      // $row['wr_from_won'] = round( $row['lanes_won'] ? ($row['won_from_won']/$row['lanes_won']) : 0, 4);
+      $result[ $ams ][ $row['hero'].'-'.$row['variant'] ] = $row;
+    }
+
+    $query_res->free_result();
+
+    // $result[ $hid ] = $r;
+
+    // echo " ";
+  // }
+
+  return $result;
+}
