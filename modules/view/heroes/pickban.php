5c6b26a17aecc1c3ba91f3cad8b87ff9baf86f3e
Lea Silakov <me@ileamare.ru>
2024-07-25 17:23:09 +0300

    wv: improve pickban sections division

--- modules/view/heroes/pickban.php
+++ modules/view/heroes/pickban.php
@@ -9,19 +9,35 @@ function rg_view_generate_heroes_pickban() {
 
   $res = "";
 
-  $res .= "<div class=\"selector-modules-level-5\">".
-    "<span class=\"selector active\">".
-      "<a href=\"?league=".$leaguetag."&mod=heroes-pickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
-        locale_string("overview").
+  $variants = [];
+  // 
+  $variants[] = "<span class=\"selector active\">".
+    "<a href=\"?league=".$leaguetag."&mod=heroes-pickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
+      locale_string("overview").
+    "</a>".
+  "</span>";
+
+  if (!empty($report['hero_variants'])) {
+    $variants[] = "<span class=\"selector\">".
+      "<a href=\"?league=".$leaguetag."&mod=heroes-variantspb".(empty($linkvars) ? "" : "&".$linkvars)."\">".
+        locale_string("variantspb").
       "</a>".
-    "</span>".
-    " | ".
-    "<span class=\"selector\">".
+    "</span>";
+  }
+
+  if (!empty($report['hero_positions'])) {
+    $variants[] = "<span class=\"selector\">".
       "<a href=\"?league=".$leaguetag."&mod=heroes-rolepickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
         locale_string("rolepickban").
       "</a>".
-    "</span>".
-  "</div>";
+    "</span>";
+  }
+
+  if (count($variants) > 1) {
+    $res .= "<div class=\"selector-modules-level-5\">".
+      implode(" | ", $variants).
+    "</div>";
+  }
 
   $res .= "<details class=\"content-text explainer\"><summary>".locale_string("explain_summary")."</summary>".
     "<div class=\"explain-content\">".
