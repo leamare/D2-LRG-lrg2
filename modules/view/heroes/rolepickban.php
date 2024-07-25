634bf1814568b1ad93d868b874452c4f5fec9e9a
Lea Silakov <me@ileamare.ru>
2024-07-26 01:24:21 +0300

    wv: add pickban variants links to rolepickban

--- modules/view/heroes/rolepickban.php
+++ modules/view/heroes/rolepickban.php
@@ -4,6 +4,8 @@ include_once($root."/modules/view/generators/pickban.php");
 
 // $modules['heroes']['rolepickban'] = "";
 
+$selectors_override['variantspb'] = 'pickban';
+
 function rg_view_generate_heroes_rolepickban() {
   global $report, $meta, $leaguetag, $linkvars;
 
@@ -31,19 +33,35 @@ function rg_view_generate_heroes_rolepickban() {
     }
   }
 
-  $res .= "<div class=\"selector-modules-level-5\">".
-    "<span class=\"selector\">".
-      "<a href=\"?league=".$leaguetag."&mod=heroes-pickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
-        locale_string("overview").
+  $variants = [];
+  // 
+  $variants[] = "<span class=\"selector\">".
+    "<a href=\"?league=".$leaguetag."&mod=heroes-pickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
+      locale_string("overview").
+    "</a>".
+  "</span>";
+
+  if (!empty($report['hero_variants'])) {
+    $variants[] = "<span class=\"selector\">".
+      "<a href=\"?league=".$leaguetag."&mod=heroes-variantspb".(empty($linkvars) ? "" : "&".$linkvars)."\">".
+        locale_string("variantspb").
       "</a>".
-    "</span>".
-    " | ".
-    "<span class=\"selector active\">".
+    "</span>";
+  }
+
+  if (!empty($report['hero_positions'])) {
+    $variants[] = "<span class=\"selector active\">".
       "<a href=\"?league=".$leaguetag."&mod=heroes-rolepickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
         locale_string("rolepickban").
       "</a>".
-    "</span>".
-  "</div>";
+    "</span>";
+  }
+
+  if (count($variants) > 1) {
+    $res .= "<div class=\"selector-modules-level-5\">".
+      implode(" | ", $variants).
+    "</div>";
+  }
 
   $res .= "<details class=\"content-text explainer\"><summary>".locale_string("explain_summary")."</summary>".
     "<div class=\"explain-content\">".
@@ -56,7 +74,7 @@ function rg_view_generate_heroes_rolepickban() {
 
   $res .= rg_generator_pickban("heroes-rolepickban", $pb, $report["random"], true, true);
 
-  $res .= rg_generator_uncontested($meta["heroes"], $report['pickban']);
+  // $res .= rg_generator_uncontested($meta["heroes"], $report['pickban']);
 
   return $res;
 }
