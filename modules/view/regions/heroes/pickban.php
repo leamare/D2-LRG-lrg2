08277d0a0833c81551e38e610ea5a6ee910641d7
Lea Silakov <me@ileamare.ru>
2024-07-28 02:01:08 +0300

    wv: add variants pickban

--- modules/view/regions/heroes/pickban.php
+++ modules/view/regions/heroes/pickban.php
@@ -3,22 +3,8 @@ include_once("$root/modules/view/generators/pickban.php");
 
 function rg_view_generate_regions_heroes_pickban($region, $reg_report) {
   global $meta, $modules, $leaguetag, $linkvars;
-
-  $res = "<div class=\"selector-modules-level-5\">".
-    "<span class=\"selector active\">".
-      "<a href=\"?league=".$leaguetag."&mod=regions-region$region-heroes-pickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
-        locale_string("overview").
-      "</a>".
-    "</span>".
-    " | ".
-    "<span class=\"selector\">".
-      "<a href=\"?league=".$leaguetag."&mod=regions-region$region-heroes-rolepickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
-        locale_string("rolepickban").
-      "</a>".
-    "</span>".
-  "</div>";
-
-  $res .= "<details class=\"content-text explainer\"><summary>".locale_string("explain_summary")."</summary>".
+  
+  $res = "<details class=\"content-text explainer\"><summary>".locale_string("explain_summary")."</summary>".
     "<div class=\"explain-content\">".
       "<div class=\"line\">".locale_string("desc_heroes_pickban")."</div>".
       "<div class=\"line\">".locale_string("desc_pickban_ranks")."</div>".
--- modules/view/regions/heroes/rolepickban.php
+++ modules/view/regions/heroes/rolepickban.php
@@ -26,21 +26,7 @@ function rg_view_generate_regions_heroes_rolepickban($region, $reg_report) {
     }
   }
 
-  $res = "<div class=\"selector-modules-level-5\">".
-    "<span class=\"selector\">".
-      "<a href=\"?league=".$leaguetag."&mod=regions-region$region-heroes-pickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
-        locale_string("overview").
-      "</a>".
-    "</span>".
-    " | ".
-    "<span class=\"selector active\">".
-      "<a href=\"?league=".$leaguetag."&mod=regions-region$region-heroes-rolepickban".(empty($linkvars) ? "" : "&".$linkvars)."\">".
-        locale_string("rolepickban").
-      "</a>".
-    "</span>".
-  "</div>";
-
-  $res .= "<details class=\"content-text explainer\"><summary>".locale_string("explain_summary")."</summary>".
+  $res = "<details class=\"content-text explainer\"><summary>".locale_string("explain_summary")."</summary>".
     "<div class=\"explain-content\">".
       "<div class=\"line\">".locale_string("desc_heroes_pickban")."</div>".
       "<div class=\"line\">".locale_string("desc_pickban_ranks")."</div>".
@@ -49,8 +35,8 @@ function rg_view_generate_regions_heroes_rolepickban($region, $reg_report) {
     "</div>".
   "</details>";
 
-  $res .= rg_generator_pickban("region$region-heroes-pickban", $pb, $reg_report['main'], true, true);
-  $res .= rg_generator_uncontested($meta["heroes"], $reg_report['pickban']);
+  $res .= rg_generator_pickban("region$region-heroes-rolepickban", $pb, $reg_report['main'], true, true);
+  // $res .= rg_generator_uncontested($meta["heroes"], $reg_report['pickban']);
 
   return $res;
 }
