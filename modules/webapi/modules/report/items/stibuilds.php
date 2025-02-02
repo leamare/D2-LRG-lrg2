0a2fdd705839eef6df78c543bc83e5c8021bf210
Lea Silakov <me@ileamare.ru>
2025-02-02 20:20:24 +0300

    wa: stibuilds api rework

--- modules/webapi/modules/report/items/stibuilds.php
+++ modules/webapi/modules/report/items/stibuilds.php
@@ -1,24 +1,46 @@
 <?php 
 
 $endpoints['items-stibuilds'] = function($mods, $vars, &$report) use (&$endpoints) {
-  if (!isset($report['starting_items']) || empty($report['starting_items']['builds']))
+  if (!isset($report['starting_items']) || empty($report['starting_items']['builds']) &&
+    (empty($report['starting_items_players']) || empty($report['starting_items_players']['builds']))
+  )
     throw new \Exception("No starting item builds data");
 
   $selected_rid = array_flip(ROLES_IDS_SIMPLE)[$vars['position'] ?? ''];
   $selected_hid = $vars['heroid'] ?? 0;
+  $selected_pid = $vars['playerid'] ?? 0;
 
-  $report['starting_items']['builds'][$selected_rid] = unwrap_data($report['starting_items']['builds'][$selected_rid]);
-  $data = $report['starting_items']['builds'][$selected_rid][$selected_hid] ?? null;
+  if (isset($vars['playerid']) || in_array("players", $mods)) {
+    if (!isset($report['starting_items_players']) || empty($report['starting_items_players']['builds']))
+      throw new \Exception("No starting item builds data for players");
+    
+    $report['starting_items_players']['builds'][$selected_rid] = unwrap_data($report['starting_items_players']['builds'][$selected_rid]);
+    $data = $report['starting_items_players']['builds'][$selected_rid][$selected_pid] ?? null;
 
-  if (empty($data)) {
-    return [];
+    if (empty($data)) {
+      return [];
+    }
+
+    $report['starting_items_players']['matches'][$selected_rid] = unwrap_data($report['starting_items_players']['matches'][$selected_rid]);
+    $matches = $report['starting_items_players']['matches'][$selected_rid][$selected_pid];
+  } else {
+    if (!isset($report['starting_items']) || empty($report['starting_items']['builds']))
+      throw new \Exception("No starting item builds data");
+
+    $report['starting_items']['builds'][$selected_rid] = unwrap_data($report['starting_items']['builds'][$selected_rid]);
+    $data = $report['starting_items']['builds'][$selected_rid][$selected_hid] ?? null;
+  
+    if (empty($data)) {
+      return [];
+    }
+  
+    $report['starting_items']['matches'][$selected_rid] = unwrap_data($report['starting_items']['matches'][$selected_rid]);
+    $matches = $report['starting_items']['matches'][$selected_rid][$selected_hid];
   }
-
-  $report['starting_items']['matches'][$selected_rid] = unwrap_data($report['starting_items']['matches'][$selected_rid]);
 
   return [
     'data' => $data,
-    'matches' => $report['starting_items']['matches'][$selected_rid][$selected_hid],
+    'matches' => $matches,
   ];
 
   return $res;
