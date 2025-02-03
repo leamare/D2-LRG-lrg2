75777d6600dc68f8cc5f8b56d77488482da27f81
Lea Silakov <me@ileamare.ru>
2025-02-03 21:42:24 +0300

    wv: added sti context blocks to players

--- modules/view/players/consumables.php
+++ modules/view/players/consumables.php
@@ -97,7 +97,67 @@ function rg_view_generate_players_sti_consumables() {
 
   $report['starting_items_players']['matches'][$selected_rid] = unwrap_data($report['starting_items_players']['matches'][$selected_rid]);
 
-  $res[$selected_tag][ROLES_IDS[$selected_rid]] = rg_generator_sticonsumables(
+  $context_info = null;
+  if ($selected_pid != 0) {
+    $pbdata = $report['players_summary'][$selected_pid] ?? [];
+
+    if (is_wrapped($report['player_laning'])) {
+      $report['player_laning'] = unwrap_data($report['player_laning']);
+    }
+    $lanedata = $report['player_laning'][0][$selected_pid] ?? [];
+
+    $context_info = [
+      'matches' => $pbdata['matches_s'] ?? 0,
+      'winrate' => $pbdata['winrate_s'] ?? 0,
+      'lane_wr' => $lanedata['lane_wr'] ?? null,
+      'role_matches' => null,
+      'role_winrate' => null,
+      'role_ratio' => null,
+    ];
+
+    if ($selected_rid != 0 && isset($report['player_positions'])) {
+      if (is_wrapped($report['player_positions'])) {
+        $report['player_positions'] = unwrap_data($report['player_positions']);
+      }
+
+      [$core, $lane] = explode('.', ROLES_IDS_SIMPLE[$selected_rid]);
+      $posdata = $report['player_positions'][$core][$lane][$selected_pid] ?? [];
+
+      $context_info['role_matches'] = $posdata['matches_s'];
+      $context_info['role_winrate'] = $posdata['winrate_s'];
+      $context_info['role_ratio'] = $posdata['matches_s']/$context_info['matches'];
+    }
+  }
+
+  $res[$selected_tag][ROLES_IDS[$selected_rid]] = "";
+
+  if ($context_info != null) {
+    $res[$selected_tag][ROLES_IDS[$selected_rid]] .= "<div class=\"content-text\">".
+      "<table id=\"stibuilds-$selected_pid-$selected_rid-context\" class=\"list\">".
+      "<thead><tr>".
+        "<th>".locale_string("player")."</th>".
+        "<th class=\"separator\">".locale_string("matches")."</th>".
+        "<th>".locale_string("winrate")."</th>".
+        "<th>".locale_string("lane_wr")."</th>".
+        "<th class=\"separator\">".locale_string("position")."</th>".
+        "<th>".locale_string("ratio")."</th>".
+        "<th>".locale_string("winrate")."</th>".
+      "</tr></thead>".
+      "<tbody>".
+        "<tr>".
+          "<td>".player_link($selected_pid)."</td>".
+          "<td class=\"separator\">".($context_info['matches'] ?? 0)."</td>".
+          "<td>".number_format(($context_info['winrate'] ?? 0)*100, 2)."%</td>".
+          "<td>".(isset($context_info['lane_wr']) ? number_format(($context_info['lane_wr'] ?? 0)*100, 2)."%" : '-')."</td>".
+          "<td class=\"separator\">".($context_info['role_matches'] ?? '-')."</td>".
+          "<td>".($context_info['role_matches'] ? number_format(($context_info['role_ratio'] ?? 0)*100, 2)."%" : '-')."</td>".
+          "<td>".($context_info['role_matches'] ? number_format(($context_info['role_winrate'] ?? 0)*100, 2)."%" : '-')."</td>".
+        "</tr>".
+      "</tbody>".
+    "</table></div>";
+  }
+
+  $res[$selected_tag][ROLES_IDS[$selected_rid]] .= rg_generator_sticonsumables(
     "items-sticonsumables-$selected_tag",
     false,
     $selected_pid,
