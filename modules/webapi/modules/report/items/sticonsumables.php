6166608080eb0c3fa078070482a5ecd1f6d05574
Lea Silakov <me@ileamare.ru>
2025-02-06 23:15:53 +0300

    wa: added context to sticonsumables

--- modules/webapi/modules/report/items/sticonsumables.php
+++ modules/webapi/modules/report/items/sticonsumables.php
@@ -10,7 +10,7 @@ $endpoints['items-sticonsumables'] = function($mods, $vars, &$report) use (&$end
   $selected_hid = $vars['heroid'] ?? 0;
   $selected_pid = $vars['playerid'] ?? 0;
 
-  $roles = array_keys($report['starting_items']['consumables']['all']);
+  $context_info = null;
 
   $data = [
     '5m' => null,
@@ -40,6 +40,35 @@ $endpoints['items-sticonsumables'] = function($mods, $vars, &$report) use (&$end
     $report['starting_items_players']['matches'][$selected_rid] = unwrap_data($report['starting_items_players']['matches'][$selected_rid]);
 
     $matches = $report['starting_items_players']['matches'][$selected_rid][$selected_pid];
+
+    $pbdata = $report['players_summary'][$selected_pid] ?? [];
+
+    if (is_wrapped($report['player_laning'])) {
+      $report['player_laning'] = unwrap_data($report['player_laning']);
+    }
+    $lanedata = $report['player_laning'][0][$selected_pid] ?? [];
+
+    $context_info = [
+      'matches' => $pbdata['matches_s'] ?? 0,
+      'winrate' => $pbdata['winrate_s'] ?? 0,
+      'lane_wr' => $lanedata['lane_wr'] ?? null,
+      'role_matches' => null,
+      'role_winrate' => null,
+      'role_ratio' => null,
+    ];
+
+    if ($selected_rid != 0 && isset($report['player_positions'])) {
+      if (is_wrapped($report['player_positions'])) {
+        $report['player_positions'] = unwrap_data($report['player_positions']);
+      }
+
+      [$core, $lane] = explode('.', ROLES_IDS_SIMPLE[$selected_rid]);
+      $posdata = $report['player_positions'][$core][$lane][$selected_hid] ?? [];
+
+      $context_info['role_matches'] = $posdata['matches_s'];
+      $context_info['role_winrate'] = $posdata['winrate_s'];
+      $context_info['role_ratio'] = $posdata['matches_s']/$context_info['matches'];
+    }
   } else if (isset($vars['team'])) {
     if (!isset($report['starting_items_players']) || empty($report['starting_items_players']['consumables']))
       throw new \Exception("No consumables data for players");
@@ -127,10 +156,43 @@ $endpoints['items-sticonsumables'] = function($mods, $vars, &$report) use (&$end
     $report['starting_items']['matches'][$selected_rid] = unwrap_data($report['starting_items']['matches'][$selected_rid]);
 
     $matches = $report['starting_items']['matches'][$selected_rid][$selected_hid];
+    
+    $pbdata = $report['pickban'][$selected_hid] ?? [];
+
+    if (is_wrapped($report['hero_laning'])) {
+      $report['hero_laning'] = unwrap_data($report['hero_laning']);
+    }
+    $lanedata = $report['hero_laning'][0][$selected_hid] ?? [];
+
+    $maindata = $report['random'] ?? $report['main'];
+
+    $context_info = [
+      'matches' => $pbdata['matches_picked'] ?? 0,
+      'winrate' => $pbdata['winrate_picked'] ?? 0,
+      'pickrate' => ($pbdata['matches_picked'] ?? 0)/$maindata['matches_total'],
+      'lane_wr' => $lanedata['lane_wr'] ?? null,
+      'role_matches' => null,
+      'role_winrate' => null,
+      'role_ratio' => null,
+    ];
+
+    if ($selected_rid != 0 && isset($report['hero_positions'])) {
+      if (is_wrapped($report['hero_positions'])) {
+        $report['hero_positions'] = unwrap_data($report['hero_positions']);
+      }
+
+      [$core, $lane] = explode('.', ROLES_IDS_SIMPLE[$selected_rid]);
+      $posdata = $report['hero_positions'][$core][$lane][$selected_hid] ?? [];
+
+      $context_info['role_matches'] = $posdata['matches_s'];
+      $context_info['role_winrate'] = $posdata['winrate_s'];
+      $context_info['role_ratio'] = $posdata['matches_s']/$context_info['matches'];
+    }
   }
 
   return [
     'data' => $data,
     'matches' => $matches,
+    'context' => $context_info,
   ];
 };
